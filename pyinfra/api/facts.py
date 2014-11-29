ec2e53be3f1be18f5a3835760cfb7d170c25de0e
Nick Barrett <nick@oxygem.com>
2014-11-29 23:50:12 +0000

    more basic facts

--- pyinfra/api/facts.py
+++ pyinfra/api/facts.py
@@ -6,24 +6,6 @@ import re
 from inspect import isclass
 
 
-# Returns the list of users
-class fact_Users:
-    command = 'cat /etc/passwd'
-
-    def process(self, output):
-        users = {}
-        for line in output.splitlines():
-            name, _, uid, gid, _, home, shell = line.split(':')
-            users[name] = {
-                'uid': uid,
-                'gid': gid,
-                'home': home,
-                'shell': shell
-            }
-
-        return users
-
-
 # Returns the Linux distribution
 # Ubuntu, CentOS & Debian currently
 class fact_Distribution:
@@ -45,11 +27,119 @@ class fact_Distribution:
                     'minor': matches.group(3)
                 }
 
+        return {'name': 'Unknown'}
 
-# class fact_NetworkDevices
+
+# Returns a dict of users -> details
+class fact_Users:
+    command = 'cat /etc/passwd'
+
+    def process(self, output):
+        users = {}
+        for line in output.splitlines():
+            name, _, uid, gid, _, home, shell = line.split(':')
+            users[name] = {
+                'uid': uid,
+                'gid': gid,
+                'home': home,
+                'shell': shell
+            }
+
+        return users
 
 
-# class fact_BlockDevices
+# Returns a dict of network devices -> details
+class fact_NetworkDevices:
+    command = 'cat /proc/net/dev'
+    regex = r'\s+([a-zA-Z0-9]+):\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)'
+
+    def process(self, output):
+        devices = {}
+        for line in output.splitlines():
+            matches = re.match(self.regex, line)
+            if matches:
+                devices[matches.group(1)] = {
+                    'receive': {
+                        'bytes': matches.group(2),
+                        'packets': matches.group(3),
+                        'errors': matches.group(4),
+                        'drop': matches.group(5),
+                        'fifo': matches.group(6),
+                        'frame': matches.group(7),
+                        'compressed': matches.group(8),
+                        'multicast': matches.group(9)
+                    },
+                    'transmit': {
+                        'bytes': matches.group(10),
+                        'packets': matches.group(11),
+                        'errors': matches.group(12),
+                        'drop': matches.group(13),
+                        'fifo': matches.group(14),
+                        'colls': matches.group(15),
+                        'carrier': matches.group(16),
+                        'compressed': matches.group(17)
+                    }
+                }
+
+        return devices
+
+
+# Returns a dict of (mounted, atm) block devices -> details
+class fact_BlockDevices:
+    command = 'df -T'
+    regex = r'([a-zA-Z0-9\/\-_]+)\s+([a-zA-Z0-9\/-_]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]+)\s+([0-9]{1,3})%\s+([a-zA-Z\/0-9\-_]+)'
+
+    def process(self, output):
+        devices = {}
+        for line in output.splitlines():
+            matches = re.match(self.regex, line)
+            if matches:
+                if matches.group(1) == 'none': continue
+                devices[matches.group(1)] = {
+                    'type': matches.group(2),
+                    'blocks': matches.group(3),
+                    'used': matches.group(4),
+                    'available': matches.group(5),
+                    'used_percent': matches.group(6),
+                    'mount': matches.group(7)
+                }
+
+        return devices
+
+
+# Returns a dict of installed apt packages -> details
+class fact_DebPackages:
+    command = 'dpkg -l'
+    regex = r'[a-z]+\s+([a-zA-Z0-9:\+\-\.]+)\s+([a-zA-Z0-9:~\.\-\+]+)\s+([a-z0-9]+)'
+
+    def process(self, output):
+        packages = {}
+        for line in output.splitlines():
+            matches = re.match(self.regex, line)
+            if matches:
+                packages[matches.group(1)] = {
+                    'version': matches.group(2)
+                }
+
+        return packages
+
+
+# Returns a dict of installed yum packages -> details
+class fact_RpmPackages:
+    command = 'rpm -qa'
+    regex = r'([a-zA-Z0-9_\-\+]+)\-([0-9a-z\.\-]+)\.([a-z0-9_]+)\.([a-z0-9_\.]+)'
+
+    def process(self, output):
+        packages = {}
+        for line in output.splitlines():
+            matches = re.match(self.regex, line)
+            if matches:
+                packages[matches.group(1)] = {
+                    'version': matches.group(2),
+                    'arch': matches.group(4)
+                }
+
+        return packages
 
 
 # Build dynamic facts & fact lists dicts
