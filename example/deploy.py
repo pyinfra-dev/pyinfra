d178beb1d00f9b1590f7eb47b3f3fa5092c3a99b
Nick Barrett <nick@oxygem.com>
2015-02-15 15:15:26 +0000

    update for latest stuff

--- example/deploy.py
+++ example/deploy.py
@@ -3,32 +3,26 @@
 # Desc: example deploy script
 
 from pyinfra import config
-from pyinfra.modules import server, linux, apt, git, pip, venv, yum, files
+from pyinfra.modules import server, linux, apt, yum# , git, pip, venv, files
 
 
 # Ensure the state of a user
 linux.user(
     'pyinfra',
     present=True,
-    home='/home/pydepoy',
+    home='/home/pyinfra',
     shell='/bin/bash',
-    sudo=True,
     public_keys=[
         'abc',
         'def'
     ],
-    delete_keys=True
-)
+    delete_keys=True,
 
-linux.user(
-    'vagrant',
-    present=True,
-    home='/home/vagrant',
-    shell='/bin/bash',
+    # Global options on all module functions (operations)
     sudo=True,
-    public_keys=[
-        'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDmecJwEqI89wKjS6dndQnTQtK6aSLiQ8X5VquTmuDTh2tCA8qelCQloQi8o8IyVYx9qTwf0Nfpa6z8+mKiSMqQ38APJebc7TkGBjkYurDHAuZNSyY1Wv7M4TiOja2v0YU+7lqeGiYCQhx0/NgxYndwrlA4wB84KwLZd/IYBORdhCNclk+WzOEpaXMtaASnkjxQRfIzAROCeqQpsUj/dXKiuUV/2csUeEGEuM9k43yE+r7LruGsj0LqXVW4/7zpu1tIaIZz6X5ilmcTRbNh6u4EOdU/b3G1Xl6R50VzHe6vzGW8l2QZiVOZwvvDoySm3zBYVO2cADkfBuhVgfHD1JbH nick@oxygem.com'
-    ]
+    sudo_user='root',
+    name='Ensure user pyinfra',
+    ignore_errors=False
 )
 
 # Ensure the state of files
@@ -48,88 +42,83 @@ linux.directory(
     user='pyinfra',
     group='pyinfra',
     permissions='755',
-    sudo=True,
-    recursive=True
-)
-
-# Generate files from local templates
-files.template(
-    'filename.jn2',
-    '/opt/wheverever/test.sh',
-    **{
-        'hello': 'world'
-    }
-)
-
-# Copy local files to remote host
-files.put(
-    'filename.txt',
-    '/home/ubuntu/filename.txt'
+    recursive=True,
+    sudo=True
 )
 
 # Work with multiple linux distributions
 if server.fact('Distribution')['name'] == 'CentOS':
     # yum package manager
-    yum.repo(
-        'name',
-        present=True
-    )
     yum.packages(
-        ['package', 'another'],
-        present=True
+        ['python-devel', 'git'],
+        present=True,
+        sudo=True
     )
 else:
     # apt package manager
-    apt.repo(
-        'app:wkgkwegk',
-        present=True
-    )
     apt.packages(
-        ['package', 'psvkshr'],
-        present=True
+        ['python-dev', 'git', 'nginx'],
+        present=True,
+        update=True,
+        sudo=True
     )
+
+# # Generate files from local templates
+# files.template(
+#     'filename.jn2',
+#     '/opt/wheverever/test.sh',
+#     **{
+#         'hello': 'world'
+#     }
+# )
+
+# # Copy local files to remote host
+# files.put(
+#     'filename.txt',
+#     '/home/ubuntu/filename.txt'
+# )
 
 # Execute arbitrary shell commands
 server.shell(
-    'nginx || sudo nginx',
+    'echo "hello"'
+)
+# # and scripts
+# server.script('''
+#     #!/bin/sh
+
+#     echo "Shell script!"
+#     exit 0
+# ''')
+# # and files
+# server.script(
+#     file='files/test.sh',
+# )
+
+# # Ensure the state of git repositories
+# git.repo(
+#     'git@github.com:Fizzadar/pyinfra.git',
+#     config.APP_DIR,
+#     update=True,
+#     branch='develop'
+# )
+
+# # Ensure the state of virtualenvs
+# venv.env(
+#     config.ENV_DIR,
+#     present=True
+# )
+
+# # Enter a virtualenv
+# with venv.enter(config.ENV_DIR):
+#     # Manage pip packages within
+#     pip.packages(
+#         requirements_file='/opt/pyinfra/requirements.pip',
+#         present=True
+#     )
+
+# Manage init.d services
+linux.init(
+    'cron',
+    restarted=True,
     ignore_errors=True
-)
-# and scripts
-server.script('''
-    #!/bin/sh
-
-    echo "Shell script!"
-    exit 0
-''')
-# and files
-server.script(
-    file='files/test.sh',
-)
-
-# Ensure the state of git repositories
-git.repo(
-    'git@github.com:Fizzadar/pyinfra.git',
-    config.APP_DIR,
-    update=True,
-    branch='develop'
-)
-
-# Ensure the state of virtualenvs
-venv.env(
-    config.ENV_DIR,
-    present=True
-)
-
-# Enter a virtualenv
-with venv.enter(config.ENV_DIR):
-    # Manage pip packages within
-    pip.packages(
-        requirements_file='/opt/pyinfra/requirements.pip',
-        present=True
-    )
-
-# Manage Linux services
-linux.service(
-    'nginx',
-    restarted=True
 )
