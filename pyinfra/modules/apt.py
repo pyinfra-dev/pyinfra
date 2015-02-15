2a0f97fc363ff8f5eb59da27a403019fcd4a4f0e
Nick Barrett <nick@oxygem.com>
2015-02-15 01:53:24 +0000

    Basic implement of yum.packages & apt.packages

--- pyinfra/modules/apt.py
+++ pyinfra/modules/apt.py
@@ -2,13 +2,30 @@
 # File: pyinfra/modules/apt.py
 # Desc: manage apt packages & repositories
 
-from pyinfra.api import op
+from pyinfra.api import operation, server
 
 
-@op
+@operation
 def repo(name, present=True):
     return []
 
-@op
-def packages(packages, present=True):
-    return []
+
+@operation
+def packages(packages, present=True, update=False, upgrade=False):
+    commands = []
+
+    if update:
+        commands.append('apt-get update')
+    if upgrade:
+        commands.append('apt-get upgrade')
+
+    current_packages = server.fact('DebPackages')
+    packages = [
+        package for package in packages
+        if package not in current_packages
+    ]
+
+    if packages:
+        commands.append('apt-get install -y {}'.format(' '.join(packages)))
+
+    return commands
--- pyinfra/modules/yum.py
+++ pyinfra/modules/yum.py
@@ -1,5 +1,26 @@
+# pyinfra
+# File: pyinfra/modules/apt.py
+# Desc: manage apt packages & repositories
+
+from pyinfra.api import operation, server
+
+
+@operation
 def repo(name, present=True):
     pass
 
-def packages(packages, present=True):
-    pass
+
+@operation
+def packages(packages, present=True, upgrade=False):
+    commands = []
+
+    current_packages = server.fact('RPMPackages')
+    packages = [
+        package for package in packages
+        if package not in current_packages
+    ]
+
+    if packages:
+        commands.append('yum install -y {}'.format(' '.join(packages)))
+
+    return commands
