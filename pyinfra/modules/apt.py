1fa5d0888a83e376ecfb312cd7c42a02e96f6d75
Nick Barrett <nick@oxygem.com>
2015-02-22 12:25:32 +0000

    +ppa function support, implement repo function

--- pyinfra/modules/apt.py
+++ pyinfra/modules/apt.py
@@ -2,27 +2,34 @@
 # File: pyinfra/modules/apt.py
 # Desc: manage apt packages & repositories
 
-from pyinfra.api import operation, operation_env, server
+from pyinfra import host
+from pyinfra.api import operation, operation_env
 
 
 @operation
 def repo(name, present=True):
     '''[Not implemented] Manage apt sources.'''
-    pass
+    return ['apt-add-repository {} -y'.format(name)]
+
+@operation
+def ppa(name, **kwargs):
+    '''[Not implemented] Shortcut for managing ppa apt sources.'''
+    return repo.__decorated__('ppa:{}'.format(name), **kwargs)
 
 
 @operation
 @operation_env(DEBIAN_FRONTEND='noninteractive') # surpresses interactive prompts
 def packages(packages, present=True, update=False, upgrade=False):
     '''Install/remove/upgrade packages & update apt.'''
+    packages = packages if isinstance(packages, list) else [packages]
     commands = []
 
     if update:
         commands.append('apt-get update')
     if upgrade:
-        commands.append('apt-get upgrade')
+        commands.append('apt-get upgrade -y')
 
-    current_packages = server.fact('DebPackages')
+    current_packages = host.deb_packages
     packages = [
         package for package in packages
         if package not in current_packages
