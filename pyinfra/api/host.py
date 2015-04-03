f7fc51956e839645713799a325acbfbeddb23d94
Nick Barrett <nick@oxygem.com>
2015-04-03 22:38:05 +0100

    Move in run_all_command and general tidy up

--- pyinfra/api/host.py
+++ pyinfra/api/host.py
@@ -12,13 +12,16 @@ import re
 from termcolor import colored
 
 import pyinfra
-from pyinfra import logger
+from pyinfra import config, logger
 
 from .facts import FACTS
-from .ssh import run_all_command
+
+LS_REGEX = re.compile(
+    '[a-z-]?([-rwx]{9})\.?\s+[0-9]+\s+([a-zA-Z]+)\s+([a-zA-Z]+)\s+([0-9]+)\s+[a-zA-Z]{3}\s+[0-9]+\s+[0-9]{2}:[0-9]{2}'
+)
 
 
-symbol_to_octal_permissions = {
+_symbol_to_octal_permissions = {
     'rwx': '7',
     'rw-': '6',
     'r-x': '5',
@@ -27,17 +30,14 @@ symbol_to_octal_permissions = {
     '-w-': '2',
     '--x': '1'
 }
-ls_regex = re.compile(
-    '[a-z-]?([-rwx]{9})\.?\s+[0-9]+\s+([a-zA-Z]+)\s+([a-zA-Z]+)\s+([0-9]+)\s+[a-zA-Z]{3}\s+[0-9]+\s+[0-9]{2}:[0-9]{2}'
-)
 def _ls_matches_to_dict(matches):
     '''Parse permissions into octal format (which is what we compare to deploy scripts).'''
     def parse_permissions(permissions):
         result = ''
         # owner, group, world
         for group in [permissions[0:3], permissions[3:6], permissions[6:9]]:
-            if group in symbol_to_octal_permissions:
-                result = '{}{}'.format(result, symbol_to_octal_permissions[group])
+            if group in _symbol_to_octal_permissions:
+                result = '{}{}'.format(result, _symbol_to_octal_permissions[group])
             else:
                 result = '{}0'.format(result)
 
@@ -64,6 +64,36 @@ def _assign_facts(outs, key, processor, accessor=None):
         logger.debug('Fact {0} for {1} assigned to {2}'.format(key, server, result))
 
 
+def _run_all_command(*args, **kwargs):
+    '''Runs a single command on all hosts in parallel.'''
+    join_output = kwargs.pop('join_output', False)
+
+    outs = [
+        (server, pyinfra._pool.spawn(pyinfra._connections[server].exec_command, *args, **kwargs))
+        for server in config.SSH_HOSTS
+    ]
+
+    # Join
+    [out[1].join() for out in outs]
+    # Get each data
+    outs = [(out[0], out[1].get()) for out in outs]
+
+    # Return the useful info
+    results = [(
+        server,
+        [line.strip() for line in stdout],
+        [line.strip() for line in stderr]
+    ) for (server, (_, stdout, stderr)) in outs]
+
+    if join_output is True:
+        results = [
+            (server, '\n'.join(stdout), '\n'.join(stderr))
+            for (server, stdout, stderr) in results
+        ]
+
+    return results
+
+
 class Host(object):
     def __getattr__(self, key):
         if key not in FACTS:
@@ -77,7 +107,7 @@ class Host(object):
 
         logger.info('Running fact {}'.format(colored(key, attrs=['bold'])))
         # For each server, spawn a job on the pool to gather the fact
-        outs = run_all_command(FACTS[key].command)
+        outs = _run_all_command(FACTS[key].command)
 
         # Process & assign each fact to pyinfra._facts
         _assign_facts(outs, key, FACTS[key].process)
@@ -94,7 +124,7 @@ class Host(object):
             return current_server_dirs[name]
 
         def parse_directory(name):
-            matches = re.match(ls_regex, name)
+            matches = re.match(LS_REGEX, name)
             if matches:
                 if not name.startswith('d'):
                     return False # indicates not directory (ie file)
@@ -102,7 +132,7 @@ class Host(object):
                 return directory
 
         logger.info('Running directory fact on {}'.format(colored(name, attrs=['bold'])))
-        outs = run_all_command('ls -ldp {0}'.format(name), join_output=True)
+        outs = _run_all_command('ls -ldp {0}'.format(name), join_output=True)
 
         # Assign all & return current
         _assign_facts(outs, name, parse_directory, '_dirs')
@@ -117,14 +147,14 @@ class Host(object):
             return current_server_files[name]
 
         def parse_file(name):
-            matches = re.match(ls_regex, name)
+            matches = re.match(LS_REGEX, name)
             if matches:
                 if name.startswith('d'):
                     return False # indicates not file (ie dir)
                 return _ls_matches_to_dict(matches)
 
         logger.info('Running file fact on {}'.format(colored(name, attrs=['bold'])))
-        outs = run_all_command('ls -ldp {0}'.format(name), join_output=True)
+        outs = _run_all_command('ls -ldp {0}'.format(name), join_output=True)
 
         # Assign & return current
         _assign_facts(outs, name, parse_file, '_files')
