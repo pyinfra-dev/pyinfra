33c1dd2327d5930c72de81cfc79b2383ffa504cb
Nick Barrett <nick@oxygem.com>
2015-07-07 22:36:12 +0100

    Remove docstring for pydocs (use __all__ instead)

--- pyinfra/modules/__init__.py
+++ pyinfra/modules/__init__.py
@@ -1,11 +1,11 @@
-'''
-+ [apt](./apt.md)
-+ [files](./files.md)
-+ [git](./git.md)
-+ [pip](./pip.md)
-+ [python](./python.md)
-+ [server](./server.md)
-+ [venv](./venv.md)
-+ [yum](./yum.md)
+# pyinfra
+# File: pyinfa/modules/__init__.py
+# Desc: Define __all__ for pydocs
 
-'''
+from os import path
+from glob import glob
+
+
+module_filenames = glob(path.join(path.dirname(__file__), '*.py'))
+module_names = [path.basename(name)[:-3] for name in module_filenames]
+__all__ = [name for name in module_names if name != '__init__']
