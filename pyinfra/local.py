a128ab88af549377e30fda7b3d51533764c9ddd2
Nick Barrett <pointlessrambler@gmail.com>
2016-01-26 00:10:15 +0000

    Fail on `local.shell` failure & nicer exception for incorrect `local.include` filename.

--- pyinfra/local.py
+++ pyinfra/local.py
@@ -7,6 +7,7 @@ from subprocess import Popen, PIPE, STDOUT
 
 from . import pseudo_state
 from .api.util import read_buffer
+from .api.exceptions import PyinfraException
 
 
 print_local = False
@@ -19,10 +20,17 @@ def set_print_local(to_print):
 
 def include(filename):
     '''
-    Execute a local python file within the ``pyinfra.pseudo_state.deploy_dir`` directory.
+    Executes a local python file within the ``pyinfra.pseudo_state.deploy_dir`` directory.
     '''
+
     filename = path.join(pseudo_state.deploy_dir, filename)
-    execfile(filename)
+
+    try:
+        execfile(filename)
+    except IOError as e:
+        raise PyinfraException(
+            'Could not include local file: {0}\n{1}'.format(filename, e)
+        )
 
 
 def shell(command, print_output=False):
@@ -40,5 +48,12 @@ def shell(command, print_output=False):
         print_output=print_output or print_local,
         print_func=lambda line: u'{0}{1}'.format(print_prefix, line)
     )
+
+    # Get & check result
+    result = process.wait()
+    if result > 0:
+        raise PyinfraException(
+            'Local command failed: {0}\n{1}'.format(command, stdout)
+        )
 
     return '\n'.join(stdout)
