1fe0b2f5c7252dec93f32440795c8c4f9d013dbc
Nick Barrett <pointlessrambler@gmail.com>
2016-02-14 11:51:20 +0000

    Move dev requirements into `setup.py`.

--- setup.py
+++ setup.py
@@ -12,22 +12,23 @@ if __name__ == '__main__':
         description='Deploy stuff by diff-ing the state you want against the remote server.',
         author='Nick / Fizzadar',
         author_email='pointlessrambler@gmail.com',
+        license='MIT',
         url='http://github.com/Fizzadar/pyinfra',
-        packages=[
+        packages=(
             'pyinfra',
             'pyinfra.api',
             'pyinfra.facts',
             'pyinfra.facts.util',
             'pyinfra.modules',
             'pyinfra.modules.util'
-        ],
+        ),
         package_dir={
             'pyinfra': 'pyinfra'
         },
-        scripts=[
-            'bin/pyinfra'
-        ],
-        install_requires=[
+        scripts=(
+            'bin/pyinfra',
+        ),
+        install_requires=(
             'gevent',
             'paramiko',
             'docopt',
@@ -35,5 +36,18 @@ if __name__ == '__main__':
             'termcolor',
             'jinja2',
             'python-dateutil'
-        ]
+        ),
+        extras_require={
+            'dev': (
+                # Dev testung requirements
+                'nose==1.3.7',
+                'jsontest==1.2',
+                'coverage==4.0.3',
+                'mock==1.3.0',
+
+                # Dev docs requirements
+                'sphinx==1.3.1',
+                'sphinx-autobuild==0.5.2'
+            )
+        }
     )
