120173892c6ce1e6d62d7f1e8d7a69059d86c498
Nick Barrett <pointlessrambler@gmail.com>
2016-03-07 21:31:27 +0000

    Simplify docs config (give up on rtd).

--- docs/conf.py
+++ docs/conf.py
@@ -2,9 +2,6 @@
 # File: docs/conf.py
 # Desc: minimal Sphinx config
 
-import os
-
-
 extensions = [
     # Official
     'sphinx.ext.autodoc',
@@ -22,15 +19,4 @@ pygments_style = 'sphinx'
 # Theme style override
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['static']
-html_style = 'theme.css'
-
-# Readthedocs appears to ignore html_style
-if os.environ.get('READTHEDOCS') == 'True':
-    html_context = {
-        'css_files': [
-            'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
-            'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
-            '_static/theme.css',
-        ],
-        'theme_css': '_static/theme.css'
-    }
+html_style = 'css/theme.css'
