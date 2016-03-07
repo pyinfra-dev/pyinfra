4eebb63d83babb885bd2496f3c5848ede4b1894e
Nick Barrett <pointlessrambler@gmail.com>
2016-03-07 21:44:54 +0000

    Attempt to abuse `!important` to sidestep latest RTD nonsense.

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,9 @@
 # File: docs/conf.py
 # Desc: minimal Sphinx config
 
+import os
+
+
 extensions = [
     # Official
     'sphinx.ext.autodoc',
@@ -19,9 +22,16 @@ pygments_style = 'sphinx'
 # Theme style override
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['static']
-html_style = 'theme.css'
+
+# Because RTD decided to add their CSS *below* the template header, in dev we have to
+# put the theme CSS after the custom CSS (so dev == rtd). But for actual RTD we want to
+# put our styles as the only CSS file, which will be included before RTDs silly overrides.
+if os.environ.get('READTHEDOCS') == 'True':
+    style_file = '_static/theme.css'
+else:
+    html_style = 'theme.css'
+    style_file = '_static/css/theme.css'
+
 html_context = {
-    'css_files': [
-        '_static/theme.css'
-    ]
+    'css_files': [style_file]
 }
