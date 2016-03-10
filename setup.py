84911a2307fb1306dbea07105c0699e08a521e42
Nick Barrett <pointlessrambler@gmail.com>
2016-03-10 01:26:01 +0000

    Dev13.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 if __name__ == '__main__':
     setup(
-        version='0.1.dev12',
+        version='0.1.dev13',
         name='pyinfra',
         description='Deploy stuff by diff-ing the state you want against the remote server.',
         author='Nick / Fizzadar',
