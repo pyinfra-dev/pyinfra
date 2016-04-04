657b5b820affd5efd4ffd07aec65595fbd1ffb68
Nick Barrett <pointlessrambler@gmail.com>
2016-04-04 12:22:35 +0100

    Change `init.d` to use distro-specific commands for enabling/disabling services with defaults.
    Supports: Ubuntu/Debian/CentOS/Fedora/RHEL/Gentoo
    
    Add `init.d_enable` for manual creation of `/etc/rcX.d/Y` links.

--- pyinfra/modules/init.py
+++ pyinfra/modules/init.py
@@ -11,7 +11,7 @@ Manages the state and configuration of init services. Support for:
 + Systemctl
 '''
 
-from pyinfra.api.operation import operation
+from pyinfra.api import operation, OperationError
 
 from . import files
 
@@ -31,14 +31,14 @@ def _handle_service_control(
     if running is False and status is not False:
         commands.append(formatter.format(name, 'stop'))
 
-    if restarted and status:
-        commands.append(formatter.format(name, 'restart'))
-
-    if reloaded and status:
-        commands.append(formatter.format(name, 'reload'))
-
     if command:
         commands.append(formatter.format(name, command))
+
+    if restarted:
+        commands.append(formatter.format(name, 'restart'))
+
+    if reloaded:
+        commands.append(formatter.format(name, 'reload'))
 
     return commands
 
@@ -47,8 +47,7 @@ def _handle_service_control(
 def d(
     state, host, name,
     running=True, restarted=False, reloaded=False,
-    command=None, enabled=None, start_priority=20, kill_priority=20,
-    start_runlevels=(2, 3, 4, 5), kill_runlevels=(0, 1, 6)
+    enabled=None, command=None
 ):
     '''
     Manage the state of SysV Init (/etc/init.d) service scripts.
@@ -57,18 +56,19 @@ def d(
     + running: whether the service should be running
     + restarted: whether the service should be restarted
     + reloaded: whether the service should be reloaded
-    + command: custom command to pass like: ``/etc/init.d/<name> <command>``
-    + enabled: whether this service should be enabled/disabled at runlevels
-    + start_priority: priority to start the service
-    + kill_priority: priority to stop the service
-    + start_runlevels: which runlevels should the service run when enabled
-    + kill_runlevels: which runlevels should the service stop when enabled
+    + enabled: whether this service should be enabled/disabled
+    + command: command (eg. reload) to run like: ``/etc/init.d/<name> <command>``
 
-    Enabled, priority & runlevels:
-        When enabled is ``True`` both priority and runlevels decide which links to init
-        scripts are pushed into ``/etc/rcX.d`` where X is the runlevel. When enabled is
-        ``False`` pyinfra removes any links matching the service name from all
-        ``/etc/rcX.d`` directories.
+    Enabled:
+        Because managing /etc/rc.d/X files is a mess, only certain Linux distributions
+        support enabling/disabling services:
+
+        + Ubuntu/Debian (``update-rc.d``)
+        + CentOS/Fedora/RHEL (``chkconfig``)
+        + Gentoo (``rc-update``)
+
+        For other distributions and more granular service control, see the
+        ``init.d_enable`` operation.
     '''
 
     commands = _handle_service_control(
@@ -77,30 +77,71 @@ def d(
         running, restarted, reloaded, command
     )
 
-    # Create desired /etc/rcX.d/<name> links
-    if enabled is True:
-        # Build link list
-        links = []
+    if enabled is not None:
+        start_links = host.fact.find_links('/etc/rc*.d/S*{0}'.format(name)) or []
 
-        for level in start_runlevels:
-            links.append('/etc/rc{0}.d/S{1}{2}'.format(level, start_priority, name))
+        # If no links exist, attempt to enable the service using distro-specific commands
+        if enabled is True and not start_links:
+            distro = host.fact.linux_distribution.get('name')
 
-        for level in kill_runlevels:
-            links.append('/etc/rc{0}.d/K{1}{2}'.format(level, kill_priority, name))
+            if distro in ('Ubuntu', 'Debian'):
+                commands.append('update-rc.d {0} defaults'.format(name))
 
-        # Ensure all the links exist
-        for link in links:
-            commands.extend(
-                files.link(state, host, link, '/etc/init.d/{0}'.format(name))
-            )
+            elif distro in ('CentOS', 'Fedora', 'Red Hat Enterprise Linux'):
+                commands.extend([
+                    'chkconfig {0} --add'.format(name),
+                    'chkconfig {0} on'.format(name)
+                ])
 
-    # Remove any /etc/rcX.d/<name> links
-    elif enabled is False:
-        links = host.fact.find_links('/etc/rc*.d/*{0}'.format(name)) or []
+            elif distro == 'Gentoo':
+                commands.append('rc-update add apache2 default')
 
-        # No state checking, just blindly remove any that exist
-        for link in links:
-            commands.append('rm -f {0}'.format(link))
+            else:
+                raise OperationError(
+                    'Unsupported: init.d with enabled on distribution: {0}'.format(distro)
+                )
+
+        # Remove any /etc/rcX.d/<name> start links
+        elif enabled is False:
+            # No state checking, just blindly remove any that exist
+            for link in start_links:
+                commands.append('rm -f {0}'.format(link))
+
+    return commands
+
+
+@operation
+def d_enable(
+    state, host, name,
+    start_priority=20, stop_priority=80,
+    start_levels=(2, 3, 4, 5), stop_levels=(0, 1, 6)
+):
+    '''
+    Manually enable /etc/init.d scripts by creating /etc/rcX.d/Y links.
+
+    + name: name of the service to enable
+    + start_priority: priority to start the service
+    + stop_priority: priority to stop the service
+    + start_levels: which runlevels should the service run when enabled
+    + stop_levels: which runlevels should the service stop when enabled
+    '''
+
+    commands = []
+
+    # Build link list
+    links = []
+
+    for level in start_levels:
+        links.append('/etc/rc{0}.d/S{1}{2}'.format(level, start_priority, name))
+
+    for level in stop_levels:
+        links.append('/etc/rc{0}.d/K{1}{2}'.format(level, stop_priority, name))
+
+    # Ensure all the new links exist
+    for link in links:
+        commands.extend(
+            files.link(state, host, link, '/etc/init.d/{0}'.format(name))
+        )
 
     return commands
 
