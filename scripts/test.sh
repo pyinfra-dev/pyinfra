f46894b1f4ebfc1c516826e6c00485942bd820d3
Nick Barrett <pointlessrambler@gmail.com>
2017-07-29 11:36:40 +0100

    Improve testing to cover the broken out `pyinfra_cli` package.

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,12 +7,20 @@ echo "### pyinfra Tests"
 # Remove any cached coverage data
 rm -f .coverage
 
-
 if [ ! -f tests/test_${TEST}.py ]; then
-    echo "--> Testing everything..."
-    nosetests --with-coverage --cover-package pyinfra $@
+    echo "--> Testing everything:"
+    ARGS="--cover-package pyinfra --cover-package pyinfra_cli $@"
 else
     echo "--> Testing ${TEST}..."
     ARGS="${@:2}"
-    nosetests tests.test_${TEST} --with-coverage --cover-package pyinfra.${TEST} $ARGS
+
+    if [ "${TEST}" = "cli" ]; then
+        ARGS="tests.test_${TEST} --cover-package pyinfra_${TEST} ${ARGS}"
+    else
+        ARGS="tests.test_${TEST} --cover-package pyinfra.${TEST} ${ARGS}"
+    fi
 fi
+
+COMMAND="nosetests --with-coverage ${ARGS}"
+echo "--> ${COMMAND}"
+$COMMAND
