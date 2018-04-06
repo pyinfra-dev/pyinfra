a23c32eab02cf97ce25b9505d1ed8ad40cba511c
Christophe Siraut <csiraut@entrouvert.com>
2018-04-06 21:32:19 +0200

    add module puppet

---
+++ pyinfra/modules/puppet.py
@@ -0,0 +1,20 @@
+from pyinfra.api import operation
+
+
+@operation
+def agent(state, host, server=None, port=None):
+    """
+    Run puppet agent
+
+    + server: master server URL
+    + port: puppet master port
+    """
+
+    args = []
+
+    if server:
+        args.append('--server=%s' % server)
+    if port:
+        args.append('--masterport=%s' % port)
+
+    yield 'puppet agent -t %s' % ' '.join(args)
