ea99d0cad1f166f382eb8d0d7a981552fa9b3923
Nick Barrett <pointlessrambler@gmail.com>
2018-10-10 12:26:16 +0100

    Add names to branches example operations.

--- example/branches.py
+++ example/branches.py
@@ -16,62 +16,62 @@ SUDO = True
 
 
 if 'debian' in host.groups:
-    server.shell('echo DEBIAN-1')
-    server.shell('echo DEBIAN-2')
+    server.shell({'DEBIAN-1'}, 'echo DEBIAN-1')
+    server.shell({'DEBIAN-2'}, 'echo DEBIAN-2')
 
 
 if 'bsd' in host.groups:
-    server.shell('echo BSD-1')
-    server.shell('echo BSD-2')
-    server.shell('echo BSD-3')
-    server.shell('echo BSD-4')
-    server.shell('echo BSD-5')
-    server.shell('echo BSD-6')
-    server.shell('echo BSD-7')
+    server.shell({'BSD-1'}, 'echo BSD-1')
+    server.shell({'BSD-2'}, 'echo BSD-2')
+    server.shell({'BSD-3'}, 'echo BSD-3')
+    server.shell({'BSD-4'}, 'echo BSD-4')
+    server.shell({'BSD-5'}, 'echo BSD-5')
+    server.shell({'BSD-6'}, 'echo BSD-6')
+    server.shell({'BSD-7'}, 'echo BSD-7')
 
 
-server.shell('echo EVERYONE-0')
+server.shell({'EVERYONE-1'}, 'echo EVERYONE-1')
 
 
 if 'bsd' in host.groups:
     server.shell('echo BSD_GROUP')
 
 
-server.shell('echo EVERYONE-1')
+server.shell({'EVERYONE-2'}, 'echo EVERYONE-2')
 
 
 if host.fact.os == 'OpenBSD':
-    server.shell('echo OS_OPENBSD')
+    server.shell({'OS_OPENBSD'}, 'echo OS_OPENBSD')
 
 
 distro = host.fact.linux_distribution['name']
 
 if distro in ('Ubuntu', 'Debian'):
-    server.shell('echo UBUNTU_DEBIAN')
+    server.shell({'UBUNTU_DEBIAN'}, 'echo UBUNTU_DEBIAN')
 
 elif distro in ('CentOS', 'Fedora'):
     if distro == 'CentOS':
-        server.shell('echo CENTOS')
-        server.shell('echo CENTOS-2')
-        server.shell('echo CENTOS-3')
-        server.shell('echo CENTOS-4')
+        server.shell({'CENTOS-1'}, 'echo CENTOS-1')
+        server.shell({'CENTOS-2'}, 'echo CENTOS-2')
+        server.shell({'CENTOS-3'}, 'echo CENTOS-3')
+        server.shell({'CENTOS-4'}, 'echo CENTOS-4')
 
-    server.shell('echo CENTOS_FEDORA')
+    server.shell({'CENTOS_FEDORA-1'}, 'echo CENTOS_FEDORA-1')
 
     if distro == 'Fedora':
-        server.shell('echo FEDORA')
+        server.shell({'FEDORA-1'}, 'echo FEDORA-1')
 
-    server.shell('echo CENTOS_FEDORA-2')
+    server.shell({'CENTOS_FEDORA-2'}, 'echo CENTOS_FEDORA-2')
 
     if distro == 'CentOS':
-        server.shell('echo CENTOS-5')
-        server.shell('echo CENTOS-6')
+        server.shell({'CENTOS-5'}, 'echo CENTOS-5')
+        server.shell({'CENTOS-6'}, 'echo CENTOS-6')
 
 if host.fact.os == 'OpenBSD':
-    server.shell('echo OPENBSD-2')
+    server.shell({'OPENBSD-2'}, 'echo OPENBSD-2')
 
 elif distro == 'Debian':
-    server.shell('echo DEBIAN')
-    server.shell('echo DEBIAN-2')
+    server.shell({'DEBIAN-3'}, 'echo DEBIAN-3')
+    server.shell({'DEBIAN-4'}, 'echo DEBIAN-4')
 
-server.shell('echo EVERYONE-2')
+server.shell({'EVERYONE-3'}, 'echo EVERYONE-3')
