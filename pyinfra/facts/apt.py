b40958dd9a34a956598aa474715e46e117575e71
Nick Barrett <pointlessrambler@gmail.com>
2019-06-26 15:59:43 -0400

    Don't fail `apt_repos` fact if `/etc/apt/sources.list.d` doesn't exist.

--- pyinfra/facts/apt.py
+++ pyinfra/facts/apt.py
@@ -56,7 +56,7 @@ class AptSources(FactBase):
         ...
     '''
 
-    command = 'cat /etc/apt/sources.list /etc/apt/sources.list.d/*.list'
+    command = 'cat /etc/apt/sources.list /etc/apt/sources.list.d/*.list || true'
     default = list
 
     def process(self, output):
