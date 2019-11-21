85940bd57c7e066fe65273ab6e4b72f70f7d5d1a
Nick Barrett <pointlessrambler@gmail.com>
2019-11-21 10:00:49 +0000

    Add Docker facts that collect `docker inspect` output.
    Adds facts:
    + `docker_system_info`
    + `docker_containers`
    + `docker_images`
    + `docker_networks`
    + `docker_container(ID)`
    + `docker_image(ID)`
    + `docker_network(ID)`

---
+++ pyinfra/facts/docker.py
@@ -0,0 +1,78 @@
+import json
+
+from pyinfra.api import FactBase
+
+
+class DockerFactBase(FactBase):
+    abstract = True
+
+    def process(self, output):
+        output = ''.join(output)
+        return json.loads(output)
+
+
+class DockerSystemInfo(DockerFactBase):
+    '''
+    Returns ``docker system info`` output in JSON format.
+    '''
+
+    command = 'docker system info --format="{{json .}}"'
+
+
+# All Docker objects
+#
+
+class DockerContainers(DockerFactBase):
+    '''
+    Returns ``docker inspect`` output for all Docker containers.
+    '''
+
+    command = 'docker container inspect `docker ps -qa`'
+
+
+class DockerImages(DockerFactBase):
+    '''
+    Returns ``docker inspect`` output for all Docker images.
+    '''
+
+    command = 'docker image inspect `docker images -q`'
+
+
+class DockerNetworks(DockerFactBase):
+    '''
+    Returns ``docker inspect`` output for all Docker networks.
+    '''
+
+    command = 'docker network inspect `docker network ls -q`'
+
+
+# Single Docker objects
+#
+
+class DockerSingleMixin(DockerFactBase):
+    def command(self, object_id):
+        return 'docker {0} inspect {1}'.format(self.docker_type, object_id)
+
+
+class DockerContainer(DockerSingleMixin):
+    '''
+    Returns ``docker inspect`` output for a single Docker container.
+    '''
+
+    docker_type = 'container'
+
+
+class DockerImage(DockerSingleMixin):
+    '''
+    Returns ``docker inspect`` output for a single Docker image.
+    '''
+
+    docker_type = 'image'
+
+
+class DockerNetwork(DockerSingleMixin):
+    '''
+    Returns ``docker inspect`` output for a single Docker network.
+    '''
+
+    docker_type = 'network'
