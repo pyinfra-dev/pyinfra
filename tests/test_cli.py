c8805abf2a98d55165aa88c8a61af52d3c39e786
Nick Barrett <pointlessrambler@gmail.com>
2019-11-23 12:14:38 +0000

    Properly test the CLI and remove redundant print tests.

---
+++ tests/test_cli.py
@@ -0,0 +1,161 @@
+from os import path
+from random import shuffle
+from unittest import TestCase
+
+from click.testing import CliRunner
+
+import pyinfra
+
+from pyinfra import pseudo_state
+from pyinfra_cli.main import _main, cli
+
+from .paramiko_util import PatchSSHTestCase
+
+
+def run_cli(*arguments):
+    pyinfra.is_cli = True
+    runner = CliRunner()
+    result = runner.invoke(cli, arguments)
+    pyinfra.is_cli = False
+    return result
+
+
+class TestCliEagerFlags(TestCase):
+    def test_print_help(self):
+        result = run_cli('--version')
+        assert result.exit_code == 0
+
+        result = run_cli('--help')
+        assert result.exit_code == 0
+
+    def test_print_facts_list(self):
+        result = run_cli('--facts')
+        assert result.exit_code == 0
+
+    def test_print_operations_list(self):
+        result = run_cli('--operations')
+        assert result.exit_code == 0
+
+
+class TestCliDeploy(PatchSSHTestCase):
+    def setUp(self):
+        pseudo_state.reset()
+
+    def test_invalid_deploy(self):
+        result = run_cli(
+            '@local',
+            'not-a-file.py',
+        )
+        assert result.exit_code == 1
+        assert 'No deploy file: not-a-file.py' in result.stdout
+
+    def test_deploy_inventory(self):
+        result = run_cli(
+            path.join('tests', 'test_deploy', 'inventory.py'),
+            'server.shell',
+            '--debug-data',
+        )
+        assert result.exit_code == 0
+
+    def test_get_facts(self):
+        result = run_cli(
+            path.join('tests', 'test_deploy', 'inventory.py'),
+            'fact',
+            'os',
+        )
+        assert result.exit_code == 0
+        assert '"somehost": null' in result.stdout
+
+    def test_deploy_operation(self):
+        result = run_cli(
+            path.join('tests', 'test_deploy', 'inventory.py'),
+            'server.shell',
+            'echo hi',
+        )
+        assert result.exit_code == 0
+
+    def test_exec_command(self):
+        result = run_cli(
+            path.join('tests', 'test_deploy', 'inventory.py'),
+            'exec',
+            '--sudo',
+            '--sudo-user', 'pyinfra',
+            '--su-user', 'pyinfrawhat',
+            '--',
+            'echo hi',
+        )
+        assert result.exit_code == 0
+
+    def test_deploy(self):
+        # Run 10 iterations of the test - each time shuffling the order of the
+        # hosts - ensuring that the ordering has no effect on the operation order.
+        for _ in range(10):
+            self._do_test_deploy()
+
+    def _do_test_deploy(self):
+        correct_op_name_and_host_names = [
+            ('First main operation', True),  # true for all hosts
+            ('Second main operation', ('somehost',)),
+            ('tests/test_deploy/a_task.py | First task operation', ('anotherhost',)),
+            ('tests/test_deploy/a_task.py | Second task operation', ('anotherhost',)),
+            ('tests/test_deploy/a_task.py | First task operation', True),
+            ('tests/test_deploy/a_task.py | Second task operation', True),
+            ('Loop-0 main operation', True),
+            ('Loop-1 main operation', True),
+            ('Third main operation', True),
+            ('Order loop 1', True),
+            ('2nd Order loop 1', True),
+            ('Order loop 2', True),
+            ('2nd Order loop 2', True),
+        ]
+
+        hosts = ['somehost', 'anotherhost', 'someotherhost']
+        shuffle(hosts)
+
+        result = run_cli(
+            ','.join(hosts),
+            path.join('tests', 'test_deploy', 'deploy.py'),
+        )
+        assert result.exit_code == 0
+
+        state = pseudo_state
+        op_order = state.get_op_order()
+
+        self.assertEqual(
+            len(correct_op_name_and_host_names), len(op_order),
+            'Incorrect number of operations detected',
+        )
+
+        for i, (correct_op_name, correct_host_names) in enumerate(
+            correct_op_name_and_host_names,
+        ):
+            op_hash = op_order[i]
+            op_meta = state.op_meta[op_hash]
+
+            self.assertEqual(list(op_meta['names'])[0], correct_op_name)
+
+            for host in state.inventory:
+                op_hashes = state.meta[host]['op_hashes']
+                if correct_host_names is True or host.name in correct_host_names:
+                    self.assertIn(op_hash, op_hashes)
+                else:
+                    self.assertNotIn(op_hash, op_hashes)
+
+
+class TestDirectMainExecution(PatchSSHTestCase):
+    '''
+    These tests are very similar as above, without the click wrappers - basically
+    here because coverage.py fails to properly detect all the code under the wrapper.
+    '''
+
+    def test_deploy_operation_direct(self):
+        with self.assertRaises(SystemExit) as e:
+            _main(
+                inventory=path.join('tests', 'test_deploy', 'inventory.py'),
+                operations=['server.shell', 'echo hi'],
+                verbosity=0, user=None, port=None, key=None, key_password=None,
+                password=None, sudo=False, sudo_user=None, su_user=None, parallel=None,
+                fail_percent=0, dry=False, limit=None, no_wait=False, serial=False,
+                debug=False, debug_data=False, debug_facts=False, debug_operations=False,
+            )
+            assert e.args == (0,)
