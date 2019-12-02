ad81bea45a119151d1b39632a5c99e3f3a4c50b7
Nick Barrett <pointlessrambler@gmail.com>
2019-12-02 16:06:13 +0000

    Add `apk` module.
    - Operations: `apk.packages`, `apk.update`, `apk.upgrade`
    - Facts: `apk_packages`

---
+++ pyinfra/facts/apk.py
@@ -0,0 +1,22 @@
+from pyinfra.api import FactBase
+
+from .util.packaging import parse_packages
+
+APK_REGEX = r'^([a-zA-Z\-]+)-([0-9\.]+\-?[a-z0-9]*)\s'
+
+
+class ApkPackages(FactBase):
+    '''
+    Returns a dict of installed apk packages:
+
+    .. code:: python
+
+        'package_name': ['version'],
+        ...
+    '''
+
+    command = 'apk list --installed'
+    deafult = dict
+
+    def process(self, output):
+        return parse_packages(APK_REGEX, output)
---
+++ pyinfra/modules/apk.py
@@ -0,0 +1,64 @@
+'''
+Mange apk packages.
+'''
+
+from pyinfra.api import operation
+
+from .util.packaging import ensure_packages
+
+
+@operation
+def upgrade(state, host):
+    '''
+    Upgrades all apk packages.
+    '''
+
+    yield 'apk upgrade'
+
+_upgrade = upgrade  # noqa: E305
+
+
+@operation
+def update(state, host):
+    '''
+    Updates apk repositories.
+    '''
+
+    yield 'apk update'
+
+_update = update  # noqa: E305
+
+
+@operation
+def packages(
+    state, host,
+    packages=None, present=True, latest=False,
+    update=False, upgrade=False,
+):
+    '''
+    Add/remove/update apk packages.
+
+    + packages: list of packages to ensure
+    + present: whether the packages should be installed
+    + latest: whether to upgrade packages without a specified version
+    + update: run apk update before installing packages
+    + upgrade: run apk upgrade before installing packages
+
+    Versions:
+        Package versions can be pinned like apk: ``<pkg>=<version>``.
+    '''
+
+    if update:
+        yield _update(state, host)
+
+    if upgrade:
+        yield _upgrade(state, host)
+
+    yield ensure_packages(
+        packages, host.fact.apk_packages, present,
+        install_command='apk add',
+        uninstall_command='apk remove',
+        upgrade_command='apk upgrade',
+        version_join='=',
+        latest=latest,
+    )
