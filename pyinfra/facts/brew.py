6256d43cf0af5ab6786b89622c134dd6c7c130dc
Nick Barrett <pointlessrambler@gmail.com>
2019-12-02 16:06:30 +0000

    Add `brew` module.
    - Operations: `brew.packages`, `brew.update`, `brew.upgrade`, `brew.casks`, `brew.cask_upgrade`, `brew.tap`
    - Facts: `brew_packages`, `brew_casks`, `brew_taps`

---
+++ pyinfra/facts/brew.py
@@ -0,0 +1,46 @@
+from pyinfra.api import FactBase
+
+from .util.packaging import parse_packages
+
+BREW_REGEX = r'^([a-zA-Z\-]+)\s([0-9\._+a-z]+)'
+
+
+class BrewPackages(FactBase):
+    '''
+    Returns a dict of installed brew packages:
+
+    .. code:: python
+
+        'package_name': ['version'],
+        ...
+    '''
+
+    command = 'brew list --versions'
+    deafult = dict
+
+    def process(self, output):
+        return parse_packages(BREW_REGEX, output)
+
+
+class BrewCasks(BrewPackages):
+    '''
+    Returns a dict of installed brew casks:
+
+    .. code:: python
+
+        'package_name': ['version'],
+        ...
+    '''
+
+    command = 'brew cask list --versions'
+
+
+class BrewTaps(FactBase):
+    '''
+    Returns a list of brew taps.
+    '''
+
+    command = 'brew tap'
+
+    def process(self, output):
+        return output
---
+++ pyinfra/modules/brew.py
@@ -0,0 +1,122 @@
+'''
+Mange brew packages.
+'''
+
+from pyinfra.api import operation
+
+from .util.packaging import ensure_packages
+
+
+@operation
+def update(state, host):
+    '''
+    Updates brew repositories.
+    '''
+
+    yield 'brew update'
+
+_update = update  # noqa: E305
+
+
+@operation
+def upgrade(state, host):
+    '''
+    Upgrades all brew packages.
+    '''
+
+    yield 'brew upgrade'
+
+_upgrade = upgrade  # noqa: E305
+
+
+@operation
+def packages(
+    state, host,
+    packages=None, present=True, latest=False,
+    update=False, upgrade=False,
+):
+    '''
+    Add/remove/update brew packages.
+
+    + packages: list of packages to ensure
+    + present: whether the packages should be installed
+    + latest: whether to upgrade packages without a specified version
+    + update: run brew update before installing packages
+    + upgrade: run brew upgrade before installing packages
+
+    Versions:
+        Package versions can be pinned like brew: ``<pkg>@<version>``.
+    '''
+
+    if update:
+        yield _update(state, host)
+
+    if upgrade:
+        yield _upgrade(state, host)
+
+    yield ensure_packages(
+        packages, host.fact.brew_packages, present,
+        install_command='brew install',
+        uninstall_command='brew uninstall',
+        upgrade_command='brew upgrade',
+        version_join='@',
+        latest=latest,
+    )
+
+
+@operation
+def cask_upgrade(state, host):
+    '''
+    Upgrades all brew casks.
+    '''
+
+    yield 'brew cask upgrade'
+
+
+@operation
+def casks(
+    state, host,
+    packages=None, present=True, latest=False, upgrade=False,
+):
+    '''
+    Add/remove/update brew casks.
+
+    + packages: list of packages to ensure
+    + present: whether the packages should be installed
+    + latest: whether to upgrade packages without a specified version
+    + upgrade: run brew cask upgrade before installing packages
+
+    Versions:
+        Package versions can be pinned like brew: ``<pkg>@<version>``.
+    '''
+
+    if upgrade:
+        yield cask_upgrade(state, host)
+
+    yield ensure_packages(
+        packages, host.fact.brew_casks, present,
+        install_command='brew cask install',
+        uninstall_command='brew cask uninstall',
+        upgrade_command='brew cask upgrade',
+        version_join='@',
+        latest=latest,
+    )
+
+
+@operation
+def tap(state, host, name, present=True):
+    '''
+    Add/remove brew taps.
+
+    + name: the name of the tasp
+    + present: whether this tap should be present or not
+    '''
+
+    taps = host.fact.brew_taps
+    is_tapped = name in taps
+
+    if present and not is_tapped:
+        yield 'brew tap {0}'.format(name)
+
+    elif not present and is_tapped:
+        yield 'brew untap {0}'.format(name)
