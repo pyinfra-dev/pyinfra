8160ce2bf079eef7ae252330bc831f1eadf39d38
Mike Kinney <mike.kinney@gmail.com>
2020-01-10 09:30:36 -0800

    use server.shell() instead of host.fact.command()

--- standalone_examples/lxd.py
+++ standalone_examples/lxd.py
@@ -4,8 +4,10 @@ from pyinfra.modules import lxd
 SUDO = True
 
 # Note: This is an example of running a command on remote
-# init lxd using all defaults
-host.fact.command('lxd init --auto')
+server.shell(
+    {'Run lxd auto init'},
+    'lxd init --auto',
+)
 
 lxd.container(
     {'Add an ubuntu container'},
