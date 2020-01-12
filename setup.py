fb96848867e269c36e220a98d8717f444486f71a
Nick Barrett <pointlessrambler@gmail.com>
2020-01-12 12:20:54 +0000

    Only install `flake8-spellcheck` when Python 3.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ TEST_REQUIRES = (
     'flake8',
     'flake8-commas',
     'flake8-quotes',
-    'flake8-spellcheck',
+    'flake8-spellcheck ; python_version >= "3"',
     'flake8-import-order',
 )
 
