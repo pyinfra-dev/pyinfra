a9cbd96e713277f34925bf53aa7bb1b33817d23c
Nick Barrett <pointlessrambler@gmail.com>
2020-01-13 09:57:34 +0000

    Add `yum_repositories` and `dnf_repositories` facts.

--- pyinfra/facts/util/packaging.py
+++ pyinfra/facts/util/packaging.py
@@ -17,3 +17,29 @@ def parse_packages(regex, output, lower=True):
             packages[name].add(matches.group(2))
 
     return packages
+
+
+def parse_yum_repositories(output):
+    repos = []
+
+    current_repo = {}
+    for line in output:
+        line = line.strip()
+        if not line:
+            continue
+
+        if line.startswith('['):
+            if current_repo:
+                repos.append(current_repo)
+                current_repo = {}
+
+            current_repo['name'] = line[1:-1]
+
+        if current_repo and '=' in line:
+            key, value = line.split('=', 1)
+            current_repo[key] = value
+
+    if current_repo:
+        repos.append(current_repo)
+
+    return repos
