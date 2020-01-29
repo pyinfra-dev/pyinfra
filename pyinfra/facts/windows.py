e9216380e68593847bf92fe35577f554657d1c68
Mike Kinney <mike.kinney@gmail.com>
2020-01-29 12:04:50 -0800

    add lots of windows facts

--- pyinfra/facts/windows.py
+++ pyinfra/facts/windows.py
@@ -46,6 +46,7 @@ class WindowsBios(FactBase):
     '''
 
     command = 'Get-CimInstance -ClassName Win32_BIOS'
+    shell_executable = 'ps'
 
     @staticmethod
     def process(output):
@@ -58,27 +59,48 @@ class WindowsBios(FactBase):
         return new_output
 
 
+def _format_windows_for_key(subject, primary_key, output):
+    '''Format the windows powershell output that uses 'Format-Line'
+       into a dict of dicts
+    '''
+    primary_key = primary_key.strip()
+    lines = {}
+    one_item = {}
+    key_value = ''
+    for line in output:
+        # split line on ':'
+        line_data = line.split(':')
+        if len(line_data) > 1:
+            # we have a data line
+            this_key = line_data[0].strip()
+            this_data = line_data[1].strip()
+            if len(line_data) > 2:
+                # there was a ':' in the data, so reconstitute the value
+                this_data = ':'.join(line_data[1:]).strip()
+            if this_key != primary_key:
+                one_item.update({this_key: this_data})
+            else:
+                key_value = this_data
+        else:
+            # we probably came across a blank line, write the line of data
+            if one_item:
+                lines[key_value] = one_item
+                one_item = {}
+                key_value = ''
+    return {subject: {primary_key: lines}}
+
+
 class WindowsProcessors(FactBase):
     '''
     Returns the processors info.
     '''
 
     command = 'Get-CimInstance -ClassName Win32_Processor | Format-List'
+    shell_executable = 'ps'
 
     @staticmethod
     def process(output):
-        procs = []
-        one_proc_data = {}
-        for line in output:
-            line_data = line.split(':')
-            if len(line_data) > 1:
-                one_proc_data.update({line_data[0].strip(): line_data[1].strip()})
-            else:
-                if one_proc_data:
-                    procs.append(one_proc_data)
-                    one_proc_data = {}
-        new_output = {'windows_processors': procs}
-        return new_output
+        return _format_windows_for_key('windows_processors', 'DeviceID', output)
 
 
 class WindowsOsVersion(FactBase):
@@ -160,42 +182,126 @@ class WindowsWhere(FactBase):
     def process(output):
         return output[0].rstrip()
 
-# TODO: Listing installed hotfixes
-# Get-CimInstance -ClassName Win32_QuickFixEngineering | format-list
 
-# TODO: Getting local drive info
-# Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" | format-list
+class WindowsHotfixes(FactBase):
+    '''
+    Returns the Windows hotfixes.
+    '''
 
-# TODO: Getting info about user logged in
-# Get-CimInstance -ClassName Win32_ComputerSystem -Property UserName
+    command = 'Get-CimInstance -ClassName Win32_QuickFixEngineering | Format-List'
+    shell_executable = 'ps'
 
-# TODO: Getting Logon Session Information
-# Get-CimInstance -ClassName Win32_LogonSession
+    @staticmethod
+    def process(output):
+        return _format_windows_for_key('windows_hotfixes', 'HotFixID', output)
 
-# TODO: Show the aliases
-# Get-Alias
 
-# TODO: help pages
-# Get-Help Get-ChildItem
-# -Detailed or -Full or -Examples
-# Update-Help ?
+class WindowsLocalDrivesInfo(FactBase):
+    '''
+    Returns the Windows local drives info.
+    '''
+
+    command = 'Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" | Format-List'
+    shell_executable = 'ps'
+
+    @staticmethod
+    def process(output):
+        return _format_windows_for_key('windows_local_drives_info', 'DeviceID', output)
+
+
+class WindowsLoggedInUserInfo(FactBase):
+    '''
+    Returns the Windows user logged in info.
+    '''
+
+    command = 'Get-CimInstance -ClassName Win32_ComputerSystem -Property UserName | Format-List'
+    shell_executable = 'ps'
+
+    @staticmethod
+    def process(output):
+        return _format_windows_for_key('windows_logged_in_user_info', 'Name', output)
+
+
+class WindowsLogonSessionInfo(FactBase):
+    '''
+    Returns the Windows user logon session info.
+    '''
+
+    command = 'Get-CimInstance -ClassName Win32_LogonSession | Format-List'
+    shell_executable = 'ps'
+
+    @staticmethod
+    def process(output):
+        return _format_windows_for_key('windows_logon_session_info', 'LogonId', output)
+
+
+class WindowsAliases(FactBase):
+    '''
+    Returns the Windows aliases.
+    '''
+
+    command = 'Get-Alias | Format-List'
+    shell_executable = 'ps'
+
+    @staticmethod
+    def process(output):
+        return _format_windows_for_key('windows_get_aliases', 'Definition', output)
+
 
 # TODO: Services (tip: 'Get-Command -Noun Service' or
 # simply 'Get-Command' to see what you can do...)
-# Get-CimInstance -ClassName Win32_Service | Format-List
 # Stop-Service -Name <name>
 # Suspend-Service -Name <name>
 # Restart-Service -Name <name>
+class WindowsServices(FactBase):
+    '''
+    Returns the Windows services.
+    '''
 
-# TODO: Ip addresses
-# Get-CimInstance -Class Win32_NetworkAdapterConfiguration | format-list
+    command = 'Get-CimInstance -ClassName Win32_Service | Format-List'
+    shell_executable = 'ps'
 
-# TODO: Processes
-# Get-Process | Format-List
-# Stop-Process <id>
+    @staticmethod
+    def process(output):
+        return _format_windows_for_key('windows_services', 'Name', output)
 
-# TODO: map network drive
-# net use B: \\FPS01\users
 
-# TODO: list windows installer applications
-# Get-CimInstance -Class Win32_Product | Format-List
+# TODO: for operations: Stop-Process <id>
+class WindowsProcesses(FactBase):
+    '''
+    Returns the Windows processes.
+    '''
+
+    command = 'Get-Process | Format-List'
+    shell_executable = 'ps'
+
+    @staticmethod
+    def process(output):
+        return _format_windows_for_key('windows_processes', 'Id', output)
+
+
+class WindowsNetworkConfiguration(FactBase):
+    '''
+    Returns the Windows network configuration.
+    '''
+
+    command = 'Get-CimInstance -Class Win32_NetworkAdapterConfiguration | Format-List'
+    shell_executable = 'ps'
+
+    @staticmethod
+    def process(output):
+        return _format_windows_for_key('windows_network_configuration', 'Index', output)
+
+
+class WindowsInstallerApplications(FactBase):
+    '''
+    Returns the Windows installer applications.
+    '''
+
+    command = 'Get-CimInstance -Class Win32_Product | Format-List'
+    shell_executable = 'ps'
+
+    @staticmethod
+    def process(output):
+        return _format_windows_for_key('windows_installer_applications',
+                                       'IdentifyingNumber', output)
