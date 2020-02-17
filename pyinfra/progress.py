4bd872bedccab79ce7824a9bb313ed046a296923
Vilhelm Prytz <vilhelm@prytznet.se>
2020-02-17 20:04:00 +0100

    Except "AttributeError" instead of "ImportError"

--- pyinfra/progress.py
+++ pyinfra/progress.py
@@ -25,7 +25,7 @@ TERMINAL_WIDTH = 0
 if IS_TTY:
     try:
         TERMINAL_WIDTH = os.get_terminal_size().columns
-    except ImportError:
+    except AttributeError:
         if not IS_WINDOWS:
             terminal_size = os.popen('stty size', 'r').read().split()
             if len(terminal_size) == 2:
