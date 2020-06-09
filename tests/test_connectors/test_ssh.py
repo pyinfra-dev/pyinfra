7902220d3dba7d9207521e7846c963a54acfab12
Nick Barrett <pointlessrambler@gmail.com>
2020-06-09 15:57:45 +0100

    Update SSH connector tests for `pyinfra-` prefixed temp filenames.

--- tests/test_connectors/test_ssh.py
+++ tests/test_connectors/test_ssh.py
@@ -334,12 +334,12 @@ class TestSSHConnector(TestCase):
 
         fake_ssh_client().exec_command.assert_called_with((
             "sudo -S -H -n -u ubuntu sh -c 'mv "
-            '/tmp/43db9984686317089fefcf2e38de527e4cb44487 '
+            '/tmp/pyinfra-43db9984686317089fefcf2e38de527e4cb44487 '
             "not-another-file && chown ubuntu not-another-file'"
         ), get_pty=False)
 
         fake_sftp_client.from_transport().putfo.assert_called_with(
-            fake_open(), '/tmp/43db9984686317089fefcf2e38de527e4cb44487',
+            fake_open(), '/tmp/pyinfra-43db9984686317089fefcf2e38de527e4cb44487',
         )
 
     @patch('pyinfra.api.connectors.ssh.SSHClient')
@@ -366,12 +366,12 @@ class TestSSHConnector(TestCase):
 
         fake_ssh_client().exec_command.assert_called_with((
             "su centos -s `which sh` -c 'mv "
-            '/tmp/43db9984686317089fefcf2e38de527e4cb44487 '
+            '/tmp/pyinfra-43db9984686317089fefcf2e38de527e4cb44487 '
             "not-another-file && chown centos not-another-file'"
         ), get_pty=False)
 
         fake_sftp_client.from_transport().putfo.assert_called_with(
-            fake_open(), '/tmp/43db9984686317089fefcf2e38de527e4cb44487',
+            fake_open(), '/tmp/pyinfra-43db9984686317089fefcf2e38de527e4cb44487',
         )
 
     @patch('pyinfra.api.connectors.ssh.SSHClient')
@@ -420,16 +420,16 @@ class TestSSHConnector(TestCase):
         fake_ssh_client().exec_command.assert_has_calls([
             call((
                 "sudo -S -H -n -u ubuntu sh -c 'cp not-a-file "
-                "/tmp/e9c0d3c8ffca943daa0e75511b0a09c84b59c508 && chmod +r not-a-file'"
+                "/tmp/pyinfra-e9c0d3c8ffca943daa0e75511b0a09c84b59c508 && chmod +r not-a-file'"
             ), get_pty=False),
             call((
                 "sudo -S -H -n -u ubuntu sh -c 'rm -f "
-                "/tmp/e9c0d3c8ffca943daa0e75511b0a09c84b59c508'"
+                "/tmp/pyinfra-e9c0d3c8ffca943daa0e75511b0a09c84b59c508'"
             ), get_pty=False),
         ])
 
         fake_sftp_client.from_transport().getfo.assert_called_with(
-            '/tmp/e9c0d3c8ffca943daa0e75511b0a09c84b59c508', fake_open(),
+            '/tmp/pyinfra-e9c0d3c8ffca943daa0e75511b0a09c84b59c508', fake_open(),
         )
 
     @patch('pyinfra.api.connectors.ssh.SSHClient')
@@ -455,7 +455,7 @@ class TestSSHConnector(TestCase):
         fake_ssh_client().exec_command.assert_has_calls([
             call((
                 "sudo -S -H -n -u ubuntu sh -c 'cp not-a-file "
-                "/tmp/e9c0d3c8ffca943daa0e75511b0a09c84b59c508 && chmod +r not-a-file'"
+                "/tmp/pyinfra-e9c0d3c8ffca943daa0e75511b0a09c84b59c508 && chmod +r not-a-file'"
             ), get_pty=False),
         ])
 
@@ -485,16 +485,16 @@ class TestSSHConnector(TestCase):
         fake_ssh_client().exec_command.assert_has_calls([
             call((
                 "sudo -S -H -n -u ubuntu sh -c 'cp not-a-file "
-                "/tmp/e9c0d3c8ffca943daa0e75511b0a09c84b59c508 && chmod +r not-a-file'"
+                "/tmp/pyinfra-e9c0d3c8ffca943daa0e75511b0a09c84b59c508 && chmod +r not-a-file'"
             ), get_pty=False),
             call((
                 "sudo -S -H -n -u ubuntu sh -c 'rm -f "
-                "/tmp/e9c0d3c8ffca943daa0e75511b0a09c84b59c508'"
+                "/tmp/pyinfra-e9c0d3c8ffca943daa0e75511b0a09c84b59c508'"
             ), get_pty=False),
         ])
 
         fake_sftp_client.from_transport().getfo.assert_called_with(
-            '/tmp/e9c0d3c8ffca943daa0e75511b0a09c84b59c508', fake_open(),
+            '/tmp/pyinfra-e9c0d3c8ffca943daa0e75511b0a09c84b59c508', fake_open(),
         )
 
     @patch('pyinfra.api.connectors.ssh.SSHClient')
@@ -522,14 +522,14 @@ class TestSSHConnector(TestCase):
         fake_ssh_client().exec_command.assert_has_calls([
             call((
                 "su centos -s `which sh` -c 'cp not-a-file "
-                "/tmp/e9c0d3c8ffca943daa0e75511b0a09c84b59c508 && chmod +r not-a-file'"
+                "/tmp/pyinfra-e9c0d3c8ffca943daa0e75511b0a09c84b59c508 && chmod +r not-a-file'"
             ), get_pty=False),
             call((
                 "su centos -s `which sh` -c 'rm -f "
-                "/tmp/e9c0d3c8ffca943daa0e75511b0a09c84b59c508'"
+                "/tmp/pyinfra-e9c0d3c8ffca943daa0e75511b0a09c84b59c508'"
             ), get_pty=False),
         ])
 
         fake_sftp_client.from_transport().getfo.assert_called_with(
-            '/tmp/e9c0d3c8ffca943daa0e75511b0a09c84b59c508', fake_open(),
+            '/tmp/pyinfra-e9c0d3c8ffca943daa0e75511b0a09c84b59c508', fake_open(),
         )
