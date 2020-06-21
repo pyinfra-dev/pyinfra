0770560d954db43b20d9ad83d7f6ca1760bf2f66
Nick Barrett <pointlessrambler@gmail.com>
2020-06-21 12:55:04 +0100

    Fix support for non-RSA private keys.
    Supports everything Paramiko can handle: RSA/DSS/ECDSA/Ed25519.

--- tests/test_connectors/test_ssh.py
+++ tests/test_connectors/test_ssh.py
@@ -85,7 +85,7 @@ class TestSSHConnector(TestCase):
 
             assert len(state.active_hosts) == 0
 
-    def test_connect_with_ssh_key(self):
+    def test_connect_with_rsa_ssh_key(self):
         state = State(make_inventory(hosts=(
             ('somehost', {'ssh_key': 'testkey'}),
         )), Config())
@@ -121,7 +121,7 @@ class TestSSHConnector(TestCase):
 
         connect_all(second_state)
 
-    def test_connect_with_ssh_key_password(self):
+    def test_connect_with_rsa_ssh_key_password(self):
         state = State(make_inventory(hosts=(
             ('somehost', {'ssh_key': 'testkey', 'ssh_key_password': 'testpass'}),
         )), Config())
@@ -149,7 +149,7 @@ class TestSSHConnector(TestCase):
             # Check the key was created properly
             fake_key_open.assert_called_with(filename='testkey', password='testpass')
 
-    def test_connect_with_ssh_key_missing_password(self):
+    def test_connect_with_rsa_ssh_key_missing_password(self):
         state = State(make_inventory(hosts=(
             ('somehost', {'ssh_key': 'testkey'}),
         )), Config())
@@ -179,7 +179,7 @@ class TestSSHConnector(TestCase):
                 'to use this key'
             )
 
-    def test_connect_with_ssh_key_wrong_password(self):
+    def test_connect_with_rsa_ssh_key_wrong_password(self):
         state = State(make_inventory(hosts=(
             ('somehost', {'ssh_key': 'testkey', 'ssh_key_password': 'testpass'}),
         )), Config())
@@ -207,6 +207,48 @@ class TestSSHConnector(TestCase):
                 connect_all(state)
 
             assert e.exception.args[0] == 'Incorrect password for private key: testkey'
+
+    def test_connect_with_dss_ssh_key(self):
+        state = State(make_inventory(hosts=(
+            ('somehost', {'ssh_key': 'testkey'}),
+        )), Config())
+
+        with patch('pyinfra.api.connectors.ssh.path.isfile', lambda *args, **kwargs: True), \
+                patch('pyinfra.api.connectors.ssh.RSAKey.from_private_key_file') as fake_rsa_key_open, \
+                patch('pyinfra.api.connectors.ssh.DSSKey.from_private_key_file') as fake_key_open:  # noqa
+
+            def fake_rsa_key_open_fail(*args, **kwargs):
+                raise SSHException
+
+            fake_rsa_key_open.side_effect = fake_rsa_key_open_fail
+
+            fake_key = FakeRSAKey()
+            fake_key_open.return_value = fake_key
+
+            state.deploy_dir = '/'
+
+            connect_all(state)
+
+            # Check the key was created properly
+            fake_key_open.assert_called_with(filename='testkey')
+
+            # And check the Paramiko SSH call was correct
+            self.fake_connect_mock.assert_called_with(
+                'somehost',
+                allow_agent=False,
+                look_for_keys=False,
+                pkey=fake_key,
+                timeout=10,
+                username='vagrant',
+            )
+
+        # Check that loading the same key again is cached in the state
+        second_state = State(make_inventory(hosts=(
+            ('somehost', {'ssh_key': 'testkey'}),
+        )), Config())
+        second_state.private_keys = state.private_keys
+
+        connect_all(second_state)
 
     def test_connect_with_missing_ssh_key(self):
         state = State(make_inventory(hosts=(
