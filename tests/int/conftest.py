93443535d8d7a22b82bbb153bfcebc84e72f0fa0
Nick Barrett <pointlessrambler@gmail.com>
2020-07-07 21:33:43 +0100

    Tidy up & fix docker/vagrant tests.

--- tests/int/conftest.py
+++ tests/int/conftest.py
@@ -1,4 +1,3 @@
-'''Common pytest integration code.'''
 import re
 import subprocess
 
@@ -7,30 +6,31 @@ import pytest
 
 class Helpers:
     @staticmethod
-    def run(command, expected_stdout_lines=None, expected_stderr_lines=None,
-            cwd='examples'):
-        '''Helper function to eliminate pytest code.'''
-        if expected_stdout_lines is None:
-            expected_stdout_lines = ['Connected', 'Starting operation', 'Errors: 0']
-        if expected_stderr_lines is None:
-            expected_stderr_lines = ['']
-        results = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE, cwd=cwd)
+    def run(command, expected_lines=None, cwd='examples'):
+        if expected_lines is None:
+            expected_lines = ['Connected', 'Starting operation', 'Errors: 0']
+
+        results = subprocess.Popen(
+            command,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            cwd=cwd,
+        )
+
         stdout, stderr = results.communicate()
+
         if type(stdout) is bytes:
             stdout = stdout.decode('utf-8')
         if type(stderr) is bytes:
             stderr = stderr.decode('utf-8')
-        assert results.returncode == 0
-        for line in expected_stdout_lines:
-            print(line)
-            assert re.search(line, stdout, re.MULTILINE)
-        for line in expected_stderr_lines:
-            print(line)
+
+        assert results.returncode == 0, stderr
+
+        for line in expected_lines:
             assert re.search(line, stderr, re.MULTILINE)
 
 
 @pytest.fixture
 def helpers():
-    '''Helper functions for testing.'''
     return Helpers
--- tests/int/test_int_docker.py
+++ tests/int/test_int_docker.py
@@ -3,72 +3,83 @@ import pytest
 
 @pytest.mark.int
 def test_int_docker_install_package_ubuntu(helpers):
-    '''Test docker'''
-    helpers.run('pyinfra @docker/ubuntu:18.04 apt.packages iftop sudo=true update=true',
-                expected_stderr_lines=['is in beta'])
+    helpers.run(
+        'pyinfra @docker/ubuntu:18.04 apt.packages iftop sudo=true update=true',
+        expected_lines=['is in beta'],
+    )
 
 
 @pytest.mark.int
 def test_int_docker_apk_on_alpine(helpers):
-    '''Test docker'''
-    helpers.run('pyinfra @docker/alpine:3.11 apk.py',
-                expected_stderr_lines=['is in beta'])
+    helpers.run(
+        'pyinfra @docker/alpine:3.11 apk.py',
+        expected_lines=['is in beta'],
+    )
 
 
 @pytest.mark.int
 def test_int_docker_apt_and_npm_on_ubuntu(helpers):
-    '''Test docker'''
-    helpers.run('pyinfra @docker/ubuntu:18.04 apt.py npm.py',
-                expected_stderr_lines=['is in beta'])
+    helpers.run(
+        'pyinfra @docker/ubuntu:18.04 apt.py npm.py',
+        expected_lines=['is in beta'],
+    )
 
 
 @pytest.mark.int
 def test_int_docker_git_gem_and_pip_on_mult(helpers):
-    '''Test docker'''
-    helpers.run('pyinfra @docker/ubuntu:18.04,@docker/centos:8,'
-                '@docker/alpine:3.11 git.py gem.py pip.py',
-                expected_stderr_lines=['is in beta'])
+    helpers.run(
+        (
+            'pyinfra '
+            '@docker/ubuntu:18.04,@docker/centos:8,@docker/alpine:3.11 '
+            'git.py gem.py pip.py'
+        ),
+        expected_lines=['is in beta'],
+    )
 
 
 @pytest.mark.int
 def test_int_docker_pacman_on_arch(helpers):
-    '''Test docker'''
-    helpers.run('pyinfra @docker/archlinux pacman.py',
-                expected_stderr_lines=['is in beta'])
+    helpers.run(
+        'pyinfra @docker/archlinux pacman.py',
+        expected_lines=['is in beta'],
+    )
 
 
 @pytest.mark.int
 def test_int_docker_files_dnf_server_on_centos(helpers):
-    '''Test docker'''
-    helpers.run('pyinfra @docker/centos:8 files.py dnf.py server.py',
-                expected_stderr_lines=['is in beta'])
+    helpers.run(
+        'pyinfra @docker/centos:8 files.py dnf.py server.py',
+        expected_lines=['is in beta'],
+    )
 
 
 @pytest.mark.int
 def test_int_docker_yum_on_centos(helpers):
-    '''Test docker'''
-    helpers.run('pyinfra @docker/centos:8 yum.py',
-                expected_stderr_lines=['is in beta'])
+    helpers.run(
+        'pyinfra @docker/centos:8 yum.py',
+        expected_lines=['is in beta'],
+    )
 
 
 @pytest.mark.int
 def test_int_docker_adhoc_apt_packages(helpers):
-    '''Test docker'''
-    helpers.run('pyinfra @docker/ubuntu:18.04 apt.packages iftop sudo=true update=true',
-                expected_stderr_lines=['is in beta'])
+    helpers.run(
+        'pyinfra @docker/ubuntu:18.04 apt.packages iftop sudo=true update=true',
+        expected_lines=['is in beta'],
+    )
 
 
 @pytest.mark.int
 def test_int_docker_adhoc_fact_os(helpers):
-    '''Test docker'''
-    helpers.run('pyinfra @docker/ubuntu:18.04,@docker/centos:8 fact os',
-                expected_stdout_lines=['Gathering facts', 'Fact data'],
-                expected_stderr_lines=['is in beta'])
+    helpers.run(
+        'pyinfra @docker/ubuntu:18.04,@docker/centos:8 fact os',
+        expected_lines=['Gathering facts', 'Fact data', 'is in beta'],
+    )
 
 
 @pytest.mark.int
 def test_int_docker_adhoc_all_facts(helpers):
-    '''Test docker'''
-    helpers.run('pyinfra @docker/ubuntu:18.04,@docker/centos:8 all-facts',
-                expected_stdout_lines=['Gathering facts', 'Loaded fact'],
-                expected_stderr_lines=['is in beta'])
+    helpers.run(
+        'pyinfra @docker/ubuntu:18.04,@docker/centos:8 all-facts',
+        expected_lines=['Gathering facts', 'Loaded fact', 'is in beta'],
+    )
