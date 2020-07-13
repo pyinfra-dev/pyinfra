acae97d863e66874fb7d57f08643f9194f98e4e2
Nick Barrett <pointlessrambler@gmail.com>
2020-07-13 22:13:12 +0100

    Add support for another `ls` date format.
    See: https://github.com/Fizzadar/pyinfra/issues/365#issuecomment-657711793
    
    This is a temporary workaround, long term fix here is to move to using
    `stat` instead of `ls` (https://github.com/Fizzadar/pyinfra/issues/358).

--- pyinfra/facts/util/files.py
+++ pyinfra/facts/util/files.py
@@ -19,6 +19,9 @@ LS_REGEX = re.compile((
     r'((?:'
     # BSD format
     r'[a-zA-Z]{3}\s+[0-9]{1,2}\s+[0-9:]{8}\s+[0-9]{4}'
+    # Other BSD format
+    # See: https://github.com/Fizzadar/pyinfra/issues/365#issuecomment-657711793
+    r'|[a-zA-Z]{3}\s+[0-9]{1,2}\s+[0-9:]{5}'
     # Or Linux format (--time-style=long-iso)
     r'|[0-9]{4}\-[0-9]{2}\-[0-9]{2}\s+[0-9:]{5}'
     # Or Linux format (BusyBox --full-time)
