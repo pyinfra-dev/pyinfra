46a9d3a7813facfdec8d4584703cb54aa3f96e92
Nick Barrett <pointlessrambler@gmail.com>
2020-07-15 13:24:06 +0100

    Add local integration tests.
    Specifically these check that running the same file/direcotry/link operation
    twice does not result in changes on the second run.

---
+++ tests/int/test_int_local.py
@@ -0,0 +1,79 @@
+from shutil import rmtree
+from tempfile import mkdtemp
+
+import pytest
+
+
+@pytest.mark.int
+@pytest.mark.local
+def test_int_local_file_no_changes(helpers):
+    temp_dir = mkdtemp()
+
+    try:
+        helpers.run(  # first run = create the file
+            'pyinfra -v @local files.file _testfile',
+            expected_lines=['@local] Success'],
+            cwd=temp_dir,
+        )
+
+        helpers.run(  # second run = no changes
+            'pyinfra -v @local files.file _testfile',
+            expected_lines=['@local] No changes'],
+            cwd=temp_dir,
+        )
+    finally:
+        rmtree(temp_dir)
+
+
+@pytest.mark.int
+@pytest.mark.local
+def test_int_local_directory_no_changes(helpers):
+    temp_dir = mkdtemp()
+
+    try:
+        helpers.run(  # first run = create the directory
+            'pyinfra -v @local files.directory _testdir',
+            expected_lines=['@local] Success'],
+            cwd=temp_dir,
+        )
+
+        helpers.run(  # second run = no changes
+            'pyinfra -v @local files.directory _testdir',
+            expected_lines=['@local] No changes'],
+            cwd=temp_dir,
+        )
+
+        helpers.run(  # third run (remove) = remove directory
+            'pyinfra -v @local files.directory _testdir present=False',
+            expected_lines=['@local] Success'],
+            cwd=temp_dir,
+        )
+
+        helpers.run(  # fourth run (remove) = no chances
+            'pyinfra -v @local files.directory _testdir present=False',
+            expected_lines=['@local] No changes'],
+            cwd=temp_dir,
+        )
+    finally:
+        rmtree(temp_dir)
+
+
+@pytest.mark.int
+@pytest.mark.local
+def test_int_local_link_no_changes(helpers):
+    temp_dir = mkdtemp()
+
+    try:
+        helpers.run(  # first run = create the link
+            'pyinfra -v @local files.link _testlink target=_testfile',
+            expected_lines=['@local] Success'],
+            cwd=temp_dir,
+        )
+
+        helpers.run(  # second run = no changes
+            'pyinfra -v @local files.link _testlink target=_testfile',
+            expected_lines=['@local] No changes'],
+            cwd=temp_dir,
+        )
+    finally:
+        rmtree(temp_dir)
