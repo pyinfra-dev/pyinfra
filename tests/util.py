9866c71bc890374851b0517d53b649da3584cd6b
Nick Barrett <pointlessrambler@gmail.com>
2020-07-27 08:49:42 +0200

    Support item assignment in fake facts.

--- tests/util.py
+++ tests/util.py
@@ -83,6 +83,9 @@ class FakeFact(object):
     def __getitem__(self, key):
         return self.data[key]
 
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
     def __contains__(self, key):
         return key in self.data
 
