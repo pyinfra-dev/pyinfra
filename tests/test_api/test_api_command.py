37ee6f11e8d38ddeadbf039639dcadb9efe4f4e6
Nick Barrett <pointlessrambler@gmail.com>
2020-08-16 12:57:16 +0100

    Expand command test coverage.

---
+++ tests/test_api/test_api_command.py
@@ -0,0 +1,66 @@
+from __future__ import print_function
+
+from unittest import TestCase
+
+from pyinfra.api import (
+    FileDownloadCommand,
+    FileUploadCommand,
+    FunctionCommand,
+    MaskString,
+    QuoteString,
+    StringCommand,
+)
+
+
+class TestStringCommand(TestCase):
+    def test_normal(self):
+        cmd = StringCommand('hello', 'world')
+        assert str(cmd) == cmd.get_raw_value() == 'hello world'
+
+    def test_masked(self):
+        cmd = StringCommand(MaskString('adsfg'))
+        assert cmd.get_raw_value() == 'adsfg'
+        assert str(cmd) == '***'
+
+    def test_mixed_masked(self):
+        cmd = StringCommand('some', 'stuff', MaskString('mask me'), 'other', 'stuff')
+        assert cmd.get_raw_value() == 'some stuff mask me other stuff'
+        assert str(cmd) == 'some stuff *** other stuff'
+
+    def test_nested(self):
+        nested_cmd = StringCommand('some', 'stuff')
+        cmd = StringCommand('hello', nested_cmd, 'world')
+        assert str(cmd) == cmd.get_raw_value() == 'hello some stuff world'
+
+    def test_quote(self):
+        quoted_str = QuoteString('quote me!')
+        cmd = StringCommand('hello', quoted_str)
+        assert str(cmd) == cmd.get_raw_value() == "hello 'quote me!'"
+
+    def test_eq(self):
+        assert StringCommand('hello world') == StringCommand('hello world')
+        with self.assertRaises(AssertionError):
+            assert StringCommand('a') == StringCommand('b')
+
+    def test_separator(self):
+        cmd = StringCommand('hello world', 'yes', separator='')
+        assert str(cmd) == 'hello worldyes'
+
+
+class TestFileCommands(TestCase):
+    def test_file_upload_command_repr(self):
+        cmd = FileUploadCommand('src', 'dest')
+        assert repr(cmd) == 'FileUploadCommand(src, dest)'
+
+    def test_file_download_command_repr(self):
+        cmd = FileDownloadCommand('src', 'dest')
+        assert repr(cmd) == 'FileDownloadCommand(src, dest)'
+
+
+class TestFunctionCommand(TestCase):
+    def test_function_command_repr(self):
+        def some_function():
+            pass
+
+        cmd = FunctionCommand(some_function, (), {})
+        assert repr(cmd) == 'FunctionCommand(some_function, (), {})'
