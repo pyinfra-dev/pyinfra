bc9fc181aff15364d3057865195ef89db465b3a3
Nick Barrett <nick@fizzadar.com>
2020-09-21 17:47:28 +0100

    Expand output test for docker integration tests.

--- tests/int/test_int_docker.py
+++ tests/int/test_int_docker.py
@@ -5,8 +5,8 @@ import pytest
 @pytest.mark.docker
 def test_int_docker_install_package_ubuntu(helpers):
     helpers.run(
-        'pyinfra -v @docker/ubuntu:18.04 apt.packages iftop sudo=true update=true',
-        expected_lines=['is in beta'],
+        'pyinfra @docker/ubuntu:18.04 apt.packages iftop sudo=true update=true',
+        expected_lines=['is in beta', 'docker build complete'],
     )
 
 
@@ -14,8 +14,8 @@ def test_int_docker_install_package_ubuntu(helpers):
 @pytest.mark.docker
 def test_int_docker_apk_on_alpine(helpers):
     helpers.run(
-        'pyinfra -v @docker/alpine:3.11 apk.py',
-        expected_lines=['is in beta'],
+        'pyinfra @docker/alpine:3.11 apk.py',
+        expected_lines=['is in beta', 'docker build complete'],
     )
 
 
@@ -23,8 +23,8 @@ def test_int_docker_apk_on_alpine(helpers):
 @pytest.mark.docker
 def test_int_docker_apt_and_npm_on_ubuntu(helpers):
     helpers.run(
-        'pyinfra -v @docker/ubuntu:18.04 apt.py npm.py',
-        expected_lines=['is in beta'],
+        'pyinfra @docker/ubuntu:18.04 apt.py npm.py',
+        expected_lines=['is in beta', 'docker build complete'],
     )
 
 
@@ -33,11 +33,11 @@ def test_int_docker_apt_and_npm_on_ubuntu(helpers):
 def test_int_docker_git_gem_and_pip_on_mult(helpers):
     helpers.run(
         (
-            'pyinfra -v '
+            'pyinfra '
             '@docker/ubuntu:18.04,@docker/centos:8,@docker/alpine:3.11 '
             'git.py gem.py pip.py'
         ),
-        expected_lines=['is in beta'],
+        expected_lines=['is in beta', 'docker build complete'],
     )
 
 
@@ -45,8 +45,8 @@ def test_int_docker_git_gem_and_pip_on_mult(helpers):
 @pytest.mark.docker
 def test_int_docker_pacman_on_arch(helpers):
     helpers.run(
-        'pyinfra -v @docker/archlinux pacman.py',
-        expected_lines=['is in beta'],
+        'pyinfra @docker/archlinux pacman.py',
+        expected_lines=['is in beta', 'docker build complete'],
     )
 
 
@@ -54,8 +54,8 @@ def test_int_docker_pacman_on_arch(helpers):
 @pytest.mark.docker
 def test_int_docker_files_dnf_server_on_centos(helpers):
     helpers.run(
-        'pyinfra -v @docker/centos:8 files.py dnf.py server.py',
-        expected_lines=['is in beta'],
+        'pyinfra @docker/centos:8 files.py dnf.py server.py',
+        expected_lines=['is in beta', 'docker build complete'],
     )
 
 
@@ -63,8 +63,8 @@ def test_int_docker_files_dnf_server_on_centos(helpers):
 @pytest.mark.docker
 def test_int_docker_yum_on_centos(helpers):
     helpers.run(
-        'pyinfra -v @docker/centos:8 yum.py',
-        expected_lines=['is in beta'],
+        'pyinfra @docker/centos:8 yum.py',
+        expected_lines=['is in beta', 'docker build complete'],
     )
 
 
@@ -72,8 +72,8 @@ def test_int_docker_yum_on_centos(helpers):
 @pytest.mark.docker
 def test_int_docker_adhoc_apt_packages(helpers):
     helpers.run(
-        'pyinfra -v @docker/ubuntu:18.04 apt.packages iftop sudo=true update=true',
-        expected_lines=['is in beta'],
+        'pyinfra @docker/ubuntu:18.04 apt.packages iftop sudo=true update=true',
+        expected_lines=['is in beta', 'docker build complete'],
     )
 
 
@@ -81,7 +81,7 @@ def test_int_docker_adhoc_apt_packages(helpers):
 @pytest.mark.docker
 def test_int_docker_adhoc_fact_os(helpers):
     helpers.run(
-        'pyinfra -v @docker/ubuntu:18.04,@docker/centos:8 fact os',
+        'pyinfra @docker/ubuntu:18.04,@docker/centos:8 fact os',
         expected_lines=['Gathering facts', 'Fact data', 'is in beta'],
     )
 
@@ -90,6 +90,6 @@ def test_int_docker_adhoc_fact_os(helpers):
 @pytest.mark.docker
 def test_int_docker_adhoc_all_facts(helpers):
     helpers.run(
-        'pyinfra -v @docker/ubuntu:18.04,@docker/centos:8 all-facts',
+        'pyinfra @docker/ubuntu:18.04,@docker/centos:8 all-facts',
         expected_lines=['Gathering facts', 'Loaded fact', 'is in beta'],
     )
