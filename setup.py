1c96fe88cbb32309b87d3252307ecb7aca7b93a3
Nick Barrett <nick@fizzadar.com>
2020-10-01 17:44:25 +0100

    Bump sphinx theme to v0.6.

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ TEST_REQUIRES = ANSIBLE_REQUIRES + (
 )
 
 DOCS_REQUIRES = (
-    'pyinfra-guzzle_sphinx_theme==0.5',
+    'pyinfra-guzzle_sphinx_theme==0.6',
     'recommonmark==0.5.0',
     'sphinx==2.2.1 ; python_version >= "3"',
 )
