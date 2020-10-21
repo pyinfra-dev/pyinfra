d7e018f9f344741c0c0705c82fcc666933dce391
Nick Barrett <nick@fizzadar.com>
2020-10-21 16:18:54 +0100

    Allow/pass functions when loading group data.

--- pyinfra_cli/inventory.py
+++ pyinfra_cli/inventory.py
@@ -1,5 +1,5 @@
 from os import listdir, path
-from types import GeneratorType
+from types import FunctionType, GeneratorType
 
 try:
     from types import SimpleNamespace
@@ -21,7 +21,7 @@ ALLOWED_HOST_TYPES = tuple(
 ALLOWED_DATA_TYPES = tuple(
     six.integer_types
     + (six.text_type, six.binary_type)
-    + (bool, dict, list, set, tuple, float, complex, SimpleNamespace),
+    + (bool, dict, list, set, tuple, float, complex, SimpleNamespace, FunctionType),
 )
 
 
