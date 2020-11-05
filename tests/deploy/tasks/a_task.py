3d2a25be8ae46e64b31c492588acebc60551a7f7
Nick Barrett <nick@fizzadar.com>
2020-11-05 19:49:03 +0000

    Add test for `preserve_loop_order` in an included file.

--- tests/deploy/tasks/a_task.py
+++ tests/deploy/tasks/a_task.py
@@ -1,3 +1,4 @@
+from pyinfra import state
 from pyinfra.operations import server
 
 
@@ -5,6 +6,17 @@ server.shell(
     name='First task operation',
     commands='echo first_task_operation',
 )
+
+with state.preserve_loop_order([1, 2]) as loop_items:
+    for item in loop_items():
+        server.shell(
+            name='Task order loop {0}'.format(item),
+            commands='echo loop_{0}'.format(item),
+        )
+        server.shell(
+            name='2nd Task order loop {0}'.format(item),
+            commands='echo loop_{0}'.format(item),
+        )
 
 server.shell(
     name='Second task operation',
