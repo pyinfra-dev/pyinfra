0f16604501da0f91bae2b3cdcd73b5b56625ebf4
Nick Barrett <nick@fizzadar.com>
2020-11-13 10:10:19 +0000

    Implement workflow + script to build docs on GitHub.

---
+++ scripts/build_public_docs.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
+
+case "${BRANCH_NAME}" in
+    "master")
+        DOCS_VERSION="latest"
+        ;;
+    "1.x")
+        DOCS_VERSION="1.x"
+        ;;
+    "0.x")
+        DOCS_VERSION="0.x"
+        ;;
+esac
+
+if [ -z "${DOCS_VERSION}" ]; then
+    echo "No docs version for this branch, noop!"
+else
+    echo "Building docs branch=${BRANCH_NAME}, version=${DOCS_VERSION}"
+    sphinx-build -a docs/ docs/public/en/$DOCS_VERSION/
+fi
