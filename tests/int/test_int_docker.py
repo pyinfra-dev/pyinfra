a03268753780e72b44713291f24752f609abc148
Nick Barrett <nick@fizzadar.com>
2020-12-05 16:30:31 +0000

    Remove beta message checks in docker int tests.

--- tests/int/test_int_docker.py
+++ tests/int/test_int_docker.py
@@ -6,7 +6,7 @@ import pytest
 def test_int_docker_install_package_ubuntu(helpers):
     helpers.run(
         'pyinfra @docker/ubuntu:18.04 apt.packages iftop sudo=true update=true',
-        expected_lines=['is in beta', 'docker build complete'],
+        expected_lines=['docker build complete'],
     )
 
 
@@ -15,7 +15,7 @@ def test_int_docker_install_package_ubuntu(helpers):
 def test_int_docker_apk_on_alpine(helpers):
     helpers.run(
         'pyinfra @docker/alpine:3.11 apk.py',
-        expected_lines=['is in beta', 'docker build complete'],
+        expected_lines=['docker build complete'],
     )
 
 
@@ -24,7 +24,7 @@ def test_int_docker_apk_on_alpine(helpers):
 def test_int_docker_apt_and_npm_on_ubuntu(helpers):
     helpers.run(
         'pyinfra @docker/ubuntu:18.04 apt.py npm.py',
-        expected_lines=['is in beta', 'docker build complete'],
+        expected_lines=['docker build complete'],
     )
 
 
@@ -37,7 +37,7 @@ def test_int_docker_git_gem_and_pip_on_mult(helpers):
             '@docker/ubuntu:18.04,@docker/centos:8,@docker/alpine:3.11 '
             'git.py gem.py pip.py'
         ),
-        expected_lines=['is in beta', 'docker build complete'],
+        expected_lines=['docker build complete'],
     )
 
 
@@ -46,7 +46,7 @@ def test_int_docker_git_gem_and_pip_on_mult(helpers):
 def test_int_docker_pacman_on_arch(helpers):
     helpers.run(
         'pyinfra @docker/archlinux pacman.py',
-        expected_lines=['is in beta', 'docker build complete'],
+        expected_lines=['docker build complete'],
     )
 
 
@@ -55,7 +55,7 @@ def test_int_docker_pacman_on_arch(helpers):
 def test_int_docker_files_dnf_server_on_centos(helpers):
     helpers.run(
         'pyinfra @docker/centos:8 files.py dnf.py server.py',
-        expected_lines=['is in beta', 'docker build complete'],
+        expected_lines=['docker build complete'],
     )
 
 
@@ -64,7 +64,7 @@ def test_int_docker_files_dnf_server_on_centos(helpers):
 def test_int_docker_yum_on_centos(helpers):
     helpers.run(
         'pyinfra @docker/centos:8 yum.py',
-        expected_lines=['is in beta', 'docker build complete'],
+        expected_lines=['docker build complete'],
     )
 
 
@@ -73,7 +73,7 @@ def test_int_docker_yum_on_centos(helpers):
 def test_int_docker_adhoc_fact_os(helpers):
     helpers.run(
         'pyinfra @docker/ubuntu:18.04,@docker/centos:8 fact os',
-        expected_lines=['Gathering facts', 'Fact data', 'is in beta'],
+        expected_lines=['Gathering facts', 'Fact data'],
     )
 
 
@@ -82,5 +82,5 @@ def test_int_docker_adhoc_fact_os(helpers):
 def test_int_docker_adhoc_all_facts(helpers):
     helpers.run(
         'pyinfra @docker/ubuntu:18.04,@docker/centos:8 all-facts',
-        expected_lines=['Gathering facts', 'Loaded fact', 'is in beta'],
+        expected_lines=['Gathering facts', 'Loaded fact'],
     )
