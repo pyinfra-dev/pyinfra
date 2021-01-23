9f4402c1d0c8a97373a8b8a7094e1c3a06bf4627
Nick Barrett <nick@fizzadar.com>
2021-01-23 16:24:08 +0000

    Tidy up facts/operation API type files.

--- pyinfra/api/operation.pyi
+++ pyinfra/api/operation.pyi
@@ -1,7 +1,32 @@
-from typing import Any, Callable, TypeVar
+from typing import Any, Callable, Optional
 
-_TFunc = TypeVar("_TFunc", bound=Callable[..., Any])
+OPERATIONS: Any
 
 
-def operation(func: _TFunc = None, pipeline_facts=None, name: str = None) -> _TFunc:
+def get_operation_names():
+    ...
+
+
+class OperationMeta:
+    commands: Any = ...
+    hash: Any = ...
+    changed: Any = ...
+
+    def __init__(self, hash: Optional[Any] = ..., commands: Optional[Any] = ...) -> None:
+        ...
+
+
+def add_op(state: Any, op_func: Any, *args: Any, **kwargs: Any):
+    ...
+
+
+def show_set_name_warning(call_location: Any) -> None:
+    ...
+
+
+def show_state_host_arguments_warning(call_location: Any) -> None:
+    ...
+
+
+def operation(func: Optional[Callable[..., Any]] = ..., pipeline_facts: Optional[Any] = ...):
     ...
