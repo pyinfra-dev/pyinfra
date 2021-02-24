48dcf46b048c94437b957616a363f9f64447b8da
Nick Barrett <nick@fizzadar.com>
2021-02-24 16:54:28 +0000

    Add todo to remove import of all operations/facts when loading pyinfra.

--- pyinfra/__init__.py
+++ pyinfra/__init__.py
@@ -17,12 +17,11 @@ from .version import __version__  # noqa
 # Trigger pseudo_* creation
 from . import pseudo_modules  # noqa
 
-# Trigger fact index creation
-from . import facts  # noqa
-
-# Trigger module imports
-from . import operations  # noqa # pragma: no cover
-
 # Initialise base classes - this sets the pseudo modules to point at the underlying
 # class objects (Host, etc), which makes ipython/etc work as expected.
 pseudo_modules.init_base_classes()
+
+# TODO: remove these! They trigger an import and index of every operation/fact. This
+# is not ideal and explicit imports are much better.
+from . import facts  # noqa
+from . import operations  # noqa
