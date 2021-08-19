c8b6b8d3e39997f3a9763321f4f74295feefcc2c
Ben Ridley <benridley29@gmail.com>
2021-08-19 16:43:29 +1000

    Remove lint

--- pyinfra/facts/selinux.py
+++ pyinfra/facts/selinux.py
@@ -1,5 +1,6 @@
 from pyinfra.api import FactBase
 
+
 class FileContext(FactBase):
     '''
     Returns structured SELinux file context data for a specified file.
@@ -18,12 +19,13 @@ class FileContext(FactBase):
 
     def process(self, output):
         context = {}
-        components = output[0].split(":")
+        components = output[0].split(':')
         context['user'] = components[0]
         context['role'] = components[1]
         context['type'] = components[2]
         context['level'] = components[3]
         return context
+
 
 class SEBoolean(FactBase):
     '''
@@ -36,7 +38,7 @@ class SEBoolean(FactBase):
 
     def command(self, boolean):
         return 'getsebool {0}'.format(boolean)
-    
+
     def process(self, output):
-        components = output[0].split(" --> ")
+        components = output[0].split(' --> ')
         return components[1]
