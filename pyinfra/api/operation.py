4d30b65080c6b2bb18175982842d04704f980baf
Nick Barrett <nick@fizzadar.com>
2021-11-16 08:44:39 +0000

    Implement legacy operation/deploy function detection, handling and warnings.

--- pyinfra/api/operation.py
+++ pyinfra/api/operation.py
@@ -23,6 +23,7 @@ from .host import Host
 from .operation_kwargs import get_execution_kwarg_keys, pop_global_op_kwargs
 from .util import (
     get_arg_value,
+    get_args_kwargs_spec,
     get_call_location,
     get_caller_frameinfo,
     get_operation_order_from_stack,
@@ -108,12 +109,12 @@ def show_set_name_warning(call_location):
 @memoize
 def show_state_host_arguments_warning(call_location):
     logger.warning((
-        '{0}:\n\tPassing `state` and `host` as the first two arguments to operations is '
-        'deprecated, please use `state` and `host` keyword arguments.'
+        '{0}:\n\tLegacy operation function detected! Operations should no longer define '
+        '`state` and `host` arguments.'
     ).format(call_location))
 
 
-def operation(func=None, pipeline_facts=None, is_idempotent=True):
+def operation(func=None, pipeline_facts=None, is_idempotent=True, _call_location=None):
     '''
     Decorator that takes a simple module function and turn it into the internal
     operation representation that consists of a list of commands + options
@@ -123,9 +124,9 @@ def operation(func=None, pipeline_facts=None, is_idempotent=True):
     # If not decorating, return function with config attached
     if func is None:
         def decorator(f):
-            setattr(f, 'pipeline_facts', pipeline_facts)
-            setattr(f, 'is_idempotent', is_idempotent)
-            return operation(f)
+            f.pipeline_facts = pipeline_facts
+            f.is_idempotent = is_idempotent
+            return operation(f, _call_location=get_call_location())
         return decorator
 
     # Index the operation!
@@ -135,6 +136,15 @@ def operation(func=None, pipeline_facts=None, is_idempotent=True):
         module_name = module_bits[-1]
         op_name = '.'.join((module_name, func.__name__))
         OPERATIONS.append(op_name)
+
+    # Check whether an operation is "legacy" - ie contains state=None, host=None kwargs
+    # TODO: remove this! COMPAT with w/<v2
+    is_legacy = False
+    args, kwargs = get_args_kwargs_spec(func)
+    if all(key in kwargs and kwargs[key] is None for key in ('state', 'host')):
+        show_state_host_arguments_warning(_call_location or get_call_location())
+        is_legacy = True
+    func.is_legacy = is_legacy
 
     # Actually decorate!
     @wraps(func)
@@ -153,6 +163,18 @@ def operation(func=None, pipeline_facts=None, is_idempotent=True):
                     'Nested operation called with global arguments: {0} ({1})'
                 ).format(global_kwarg_keys, get_call_location()))
             return func(*args, **kwargs) or []
+
+        # If this is a legacy operation function (ie - state & host arg kwargs), ensure that state
+        # and host are included as kwargs.
+        if func.is_legacy:
+            if 'state' not in kwargs:
+                kwargs['state'] = state
+            if 'host' not in kwargs:
+                kwargs['host'] = host
+        # If not legacy, pop off any state/host kwargs that may come from legacy @deploy functions
+        else:
+            kwargs.pop('state', None)
+            kwargs.pop('host', None)
 
         # Name the operation
         name = global_kwargs.get('name')
