5782a89cc3a32485e8b9646a6de5980d8c6f1699
Nick Mills-Barrett <nick@fizzadar.com>
2022-02-23 09:05:47 +0000

    Remove deprecated `pyinfra.modules` import.

--- pyinfra/modules.py
+++
@@ -1,7 +0,0 @@
-from pyinfra import logger
-from pyinfra.operations import *  # noqa: F401,F403
-from pyinfra.operations import __all__
-
-__all__ = __all__
-
-logger.warning('Use of `pyinfra.modules` is deprecated, please use `pyinfra.operations`.')
