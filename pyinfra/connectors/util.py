720ca8e07e5a77f987e21212115f6aa40d06b498
Luna <git@l4.pm>
2022-03-08 13:37:26 -0300

    Escape exported variables

--- pyinfra/connectors/util.py
+++ pyinfra/connectors/util.py
@@ -264,7 +264,7 @@ def make_unix_command(
 
     if env:
         env_string = ' '.join([
-            '{0}={1}'.format(key, value)
+            '"{0}={1}"'.format(key, value)
             for key, value in env.items()
         ])
         command = StringCommand('export', env_string, '&&', command)
