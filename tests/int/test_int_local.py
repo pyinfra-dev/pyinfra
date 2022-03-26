6d2b69881fd8f20261b68a6ac7dde39f05f5e81f
Nick Mills-Barrett <nick@fizzadar.com>
2022-03-26 15:58:40 +0000

    Use fixture to wrap temporary directories in local e2e tests

--- tests/int/test_int_local.py
+++ tests/int/test_int_local.py
@@ -8,112 +8,101 @@ from tempfile import mkdtemp
 import pytest
 
 
-@pytest.mark.int
-@pytest.mark.local
-def test_int_local_file_no_changes(helpers):
+@pytest.fixture()
+def temp_dir():
     temp_dir = mkdtemp()
-
     try:
-        helpers.run_check_output(  # first run = create the file
-            'pyinfra -v @local files.file _testfile',
-            expected_lines=['@local] Success'],
-            cwd=temp_dir,
-        )
-
-        helpers.run_check_output(  # second run = no changes
-            'pyinfra -v @local files.file _testfile',
-            expected_lines=['@local] No changes'],
-            cwd=temp_dir,
-        )
+        yield temp_dir
     finally:
         rmtree(temp_dir)
 
 
 @pytest.mark.int
 @pytest.mark.local
-def test_int_local_directory_no_changes(helpers):
-    temp_dir = mkdtemp()
+def test_int_local_file_no_changes(helpers, temp_dir):
+    helpers.run_check_output(  # first run = create the file
+        'pyinfra -v @local files.file _testfile',
+        expected_lines=['@local] Success'],
+        cwd=temp_dir,
+    )
 
-    try:
-        helpers.run_check_output(  # first run = create the directory
-            'pyinfra -v @local files.directory _testdir',
-            expected_lines=['@local] Success'],
-            cwd=temp_dir,
-        )
-
-        helpers.run_check_output(  # second run = no changes
-            'pyinfra -v @local files.directory _testdir',
-            expected_lines=['@local] No changes'],
-            cwd=temp_dir,
-        )
-
-        helpers.run_check_output(  # third run (remove) = remove directory
-            'pyinfra -v @local files.directory _testdir present=False',
-            expected_lines=['@local] Success'],
-            cwd=temp_dir,
-        )
-
-        helpers.run_check_output(  # fourth run (remove) = no chances
-            'pyinfra -v @local files.directory _testdir present=False',
-            expected_lines=['@local] No changes'],
-            cwd=temp_dir,
-        )
-    finally:
-        rmtree(temp_dir)
+    helpers.run_check_output(  # second run = no changes
+        'pyinfra -v @local files.file _testfile',
+        expected_lines=['@local] No changes'],
+        cwd=temp_dir,
+    )
 
 
 @pytest.mark.int
 @pytest.mark.local
-def test_int_local_link_no_changes(helpers):
-    temp_dir = mkdtemp()
+def test_int_local_directory_no_changes(helpers, temp_dir):
+    helpers.run_check_output(  # first run = create the directory
+        'pyinfra -v @local files.directory _testdir',
+        expected_lines=['@local] Success'],
+        cwd=temp_dir,
+    )
 
-    try:
-        helpers.run_check_output(  # first run = create the link
-            'pyinfra -v @local files.link _testlink target=_testfile',
-            expected_lines=['@local] Success'],
-            cwd=temp_dir,
-        )
+    helpers.run_check_output(  # second run = no changes
+        'pyinfra -v @local files.directory _testdir',
+        expected_lines=['@local] No changes'],
+        cwd=temp_dir,
+    )
 
-        helpers.run_check_output(  # second run = no changes
-            'pyinfra -v @local files.link _testlink target=_testfile',
-            expected_lines=['@local] No changes'],
-            cwd=temp_dir,
-        )
-    finally:
-        rmtree(temp_dir)
+    helpers.run_check_output(  # third run (remove) = remove directory
+        'pyinfra -v @local files.directory _testdir present=False',
+        expected_lines=['@local] Success'],
+        cwd=temp_dir,
+    )
+
+    helpers.run_check_output(  # fourth run (remove) = no chances
+        'pyinfra -v @local files.directory _testdir present=False',
+        expected_lines=['@local] No changes'],
+        cwd=temp_dir,
+    )
 
 
 @pytest.mark.int
 @pytest.mark.local
-def test_int_local_line_no_changes(helpers):
-    temp_dir = mkdtemp()
+def test_int_local_link_no_changes(helpers, temp_dir):
+    helpers.run_check_output(  # first run = create the link
+        'pyinfra -v @local files.link _testlink target=_testfile',
+        expected_lines=['@local] Success'],
+        cwd=temp_dir,
+    )
 
-    try:
-        helpers.run_check_output(  # first run = create the line
-            'pyinfra -v @local files.line _testfile someline',
-            expected_lines=['@local] Success'],
-            cwd=temp_dir,
-        )
+    helpers.run_check_output(  # second run = no changes
+        'pyinfra -v @local files.link _testlink target=_testfile',
+        expected_lines=['@local] No changes'],
+        cwd=temp_dir,
+    )
 
-        helpers.run_check_output(  # second run = no changes
-            'pyinfra -v @local files.line _testfile someline',
-            expected_lines=['@local] No changes'],
-            cwd=temp_dir,
-        )
 
-        helpers.run_check_output(  # replace the line
-            'pyinfra -v @local files.line _testfile someline replace=anotherline',
-            expected_lines=['@local] Success'],
-            cwd=temp_dir,
-        )
+@pytest.mark.int
+@pytest.mark.local
+def test_int_local_line_no_changes(helpers, temp_dir):
+    helpers.run_check_output(  # first run = create the line
+        'pyinfra -v @local files.line _testfile someline',
+        expected_lines=['@local] Success'],
+        cwd=temp_dir,
+    )
 
-        helpers.run_check_output(  # second run replace the line = no changes
-            'pyinfra -v @local files.line _testfile someline replace=anotherline',
-            expected_lines=['@local] No changes'],
-            cwd=temp_dir,
-        )
-    finally:
-        rmtree(temp_dir)
+    helpers.run_check_output(  # second run = no changes
+        'pyinfra -v @local files.line _testfile someline',
+        expected_lines=['@local] No changes'],
+        cwd=temp_dir,
+    )
+
+    helpers.run_check_output(  # replace the line
+        'pyinfra -v @local files.line _testfile someline replace=anotherline',
+        expected_lines=['@local] Success'],
+        cwd=temp_dir,
+    )
+
+    helpers.run_check_output(  # second run replace the line = no changes
+        'pyinfra -v @local files.line _testfile someline replace=anotherline',
+        expected_lines=['@local] No changes'],
+        cwd=temp_dir,
+    )
 
 
 @pytest.mark.int
