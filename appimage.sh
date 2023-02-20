05437c17169a8a0c7cbb062201235c4798792c82
Phyo <phyoakl@hexcode.tech>
2023-02-20 14:51:40 +0630

    used pipx instead of pip for python-appimage

--- appimage.sh
+++ appimage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pip install python-appimage
+pipx install python-appimage
 python-appimage install appimagetool &&
 python-appimage install patchelf &&
 python-appimage build app -p 3.11 ./appimage
