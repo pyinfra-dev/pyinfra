62a8f081279779c4f1eed246139f615cf5fed642
Peder Bergebakken Sundt <pbsds@hotmail.com>
2023-09-23 04:36:54 +0200

    Bump paramiko major version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ using a package manager (apt, yum, etc), normally named: python-setuptools
 
 INSTALL_REQUIRES = (
     "gevent>=1.5",
-    "paramiko>=2.7,<3",  # 2.7 (2019) adds OpenSSH key format + Match SSH config
+    "paramiko>=2.7,<4",  # 2.7 (2019) adds OpenSSH key format + Match SSH config
     "click>2",
     "colorama<1",  # Windows color support for click
     "jinja2>2,<4",
