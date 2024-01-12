395bc5fe6992d5b8cbfb1dcd6381e5d617e8c460
Nick Mills-Barrett <nick@fizzadar.com>
2024-01-12 18:43:52 +0000

    Fix import

--- pyinfra/operations/util/__init__.py
+++ pyinfra/operations/util/__init__.py
@@ -1,7 +1,7 @@
 from typing import TYPE_CHECKING, Callable
 
 if TYPE_CHECKING:
-    from pyinfra.api.operation_util import OperationMeta
+    from pyinfra.api.operation import OperationMeta
 
 
 def any_changed(*args: "OperationMeta") -> Callable[[], bool]:
