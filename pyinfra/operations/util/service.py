96922637fc479909fa68582f44afe7c65a6f5b99
Nick Mills-Barrett <nick@fizzadar.com>
2024-01-21 12:46:01 +0000

    Remove at-execution script in service operations
    This is no longer required with the v3 eval-facts-at-runtime changes!

--- pyinfra/operations/util/service.py
+++ pyinfra/operations/util/service.py
@@ -11,42 +11,27 @@ def handle_service_control(
 ):
     status = statuses.get(name, None)
 
-    # If we don't know the status, we need to check if it's up before starting
-    # and/or restarting/reloading
-    if status is None:
-        yield (
-            "if ({status_command}); then "
-            "({stop_command}); ({restart_command}); ({reload_command}); "
-            "else ({start_command}); fi"
-        ).format(
-            status_command=formatter.format(name, status_argument),
-            start_command=(formatter.format(name, "start") if running is True else "true"),
-            stop_command=(formatter.format(name, "stop") if running is False else "true"),
-            restart_command=(formatter.format(name, "restart") if restarted else "true"),
-            reload_command=(formatter.format(name, "reload") if reloaded else "true"),
-        )
-    else:
-        # Need down but running
-        if running is False:
-            if status:
-                yield formatter.format(name, "stop")
-            else:
-                host.noop("service {0} is stopped".format(name))
+    # Need down but running
+    if running is False:
+        if status:
+            yield formatter.format(name, "stop")
+        else:
+            host.noop("service {0} is stopped".format(name))
 
-        # Need running but down
-        if running is True:
-            if not status:
-                yield formatter.format(name, "start")
-            else:
-                host.noop("service {0} is running".format(name))
+    # Need running but down
+    if running is True:
+        if not status:
+            yield formatter.format(name, "start")
+        else:
+            host.noop("service {0} is running".format(name))
 
-        # Only restart if the service is already running
-        if restarted and status:
-            yield formatter.format(name, "restart")
+    # Only restart if the service is already running
+    if restarted and status:
+        yield formatter.format(name, "restart")
 
-        # Only reload if the service is already reloaded
-        if reloaded and status:
-            yield formatter.format(name, "reload")
+    # Only reload if the service is already reloaded
+    if reloaded and status:
+        yield formatter.format(name, "reload")
 
     # Always execute arbitrary commands as these may or may not rely on the service
     # being up or down
