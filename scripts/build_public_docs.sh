fcf631da2063c051f996b28c093a161a2da82a8d
Nick Mills-Barrett <nick@fizzadar.com>
2024-01-23 21:12:12 +0000

    Update docs script

--- scripts/build_public_docs.sh
+++ scripts/build_public_docs.sh
@@ -2,22 +2,24 @@
 
 set -euo pipefail
 
+LATEST_NAME="3.x"
+CURRENT_NAME="2.x"
+
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 TAG_NAME=$(git tag --points-at HEAD)
 
-if [ ! "${BRANCH_NAME}" = "2.x" ]; then
-    2>&1 echo "Not on 2.x branch, bailing!"
-    exit 1
+if [ "${BRANCH_NAME}" = "${LATEST_NAME}" ]; then
+    echo "Building latest docs (${LATEST_NAME})"
+    env DOCS_VERSION=latest sphinx-build -a docs/ docs/public/en/latest/
 fi
 
-echo "Building latest docs (2.x branch HEAD)"
-env DOCS_VERSION=latest sphinx-build -a docs/ docs/public/en/latest/
-
-if [ -n "${TAG_NAME}" ] && [[ "$TAG_NAME" =~ ^v2\.[0-9]+(\.[0-9]+)?$ ]]; then
-    echo "Building 2.x docs for tag: ${TAG_NAME}"
-    env DOCS_VERSION=2.x sphinx-build -a docs/ docs/public/en/2.x/
-    echo "Generating /page redirects"
-    env DOCS_VERSION=$DOCS_VERSION python scripts/generate_redirect_pages.py
+if [ "${BRANCH_NAME}" = "${CURRENT_NAME}" ]; then
+    if [ -n "${TAG_NAME}" ] && [[ "$TAG_NAME" =~ ^v3\.[0-9]+(\.[0-9]+)?$ ]]; then
+        echo "Building ${BRANCH_NAME} docs for tag: ${TAG_NAME}"
+        env DOCS_VERSION=$BRANCH_NAME sphinx-build -a docs/ docs/public/en/$BRANCH_NAME/
+        echo "Generating /page redirects"
+        env DOCS_VERSION=$DOCS_VERSION python scripts/generate_redirect_pages.py
+    fi
 fi
 
 echo "Docs build complete"
