688c8f56a81f91fbf84685a4796dd99c7f345161
Marten Lienen <marten.lienen@gmail.com>
2024-05-23 22:23:07 +0200

    Update minimum python version to 3.8

--- setup.py
+++ setup.py
@@ -128,6 +128,7 @@ if __name__ == "__main__":
                 "vagrant = pyinfra.connectors.vagrant:VagrantInventoryConnector",
             ],
         },
+        python_requires=">=3.8",
         install_requires=INSTALL_REQUIRES,
         extras_require={
             "test": TEST_REQUIRES,
@@ -144,7 +145,6 @@ if __name__ == "__main__":
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
             "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
