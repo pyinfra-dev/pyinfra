7cfa2398d85109497b8716ad2eea092a4825ba05
Loïc Tosser <loic@kalvad.com>
2025-03-12 09:37:04 +0400

    Simplify LinuxDistribution fact release metadata parsing
    Refactor the release metadata dictionary comprehension to a single line, removing unnecessary line breaks while preserving the original logic of converting OS release info keys to uppercase.

--- pyinfra/facts/server.py
+++ pyinfra/facts/server.py
@@ -581,9 +581,7 @@ class LinuxDistribution(FactBase[LinuxDistributionDict]):
                 include_uname=False,
             )
 
-            release_meta = {
-                key.upper(): value for key, value in parsed.os_release_info().items()
-            }
+            release_meta = {key.upper(): value for key, value in parsed.os_release_info().items()}
             # Distro 1.7+ adds this, breaking tests
             # TODO: fix this!
             release_meta.pop("RELEASE_CODENAME", None)
