d5f27e569959ca0000257df04bd2ac9241927260
Loïc Tosser <loic@kalvad.com>
2026-03-10 21:02:19 +0100

    Update src/pyinfra/facts/server.py
    Co-authored-by: Nick Mills-Barrett <nick@fizzadar.com>

--- src/pyinfra/facts/server.py
+++ src/pyinfra/facts/server.py
@@ -968,7 +968,7 @@ class SecurityLimits(FactBase):
 
     @override
     def command(self):
-        return "cat /etc/security/limits.conf 2>/dev/null || true"
+        return "! test -e /etc/security/limits.conf || cat /etc/security/limits.conf"
 
     default = list
 
