pyinfra-dev/pyinfra

commit 5698bf182a7b00e0fcc7ce0125c7460902004286

feat(facts.zfs): add check_preconditions for ZfsDatasets and update command method

src/pyinfra/facts/zfs.py
@@ -35,14 +35,22 @@ class ZfsPools(FactBase):
 
 class ZfsDatasets(FactBase):
     @override
-    def command(self) -> str:
-        return "zfs get -H all"
-
-    @override
     def requires_command(self) -> str:
         return "zfs"
 
     default = dict
+
+    @override
+    def check_preconditions(self, state, host):
+        from pyinfra.facts.server import KernelModules
+
+        modules = host.get_fact(KernelModules) or {}
+        if "zfs" not in modules:
+            return "kernel module 'zfs' is not loaded"
+
+    @override
+    def command(self) -> str:
+        return "zfs get -H all"
 
     @override
     def process(self, output):

commit f153e0d075571ee6bbe3a330d0e4c6e1ec2111e2

fix(facts.zfs,facts.server.KernelModules): commit 5698bf1 broke zfs on freebsd

src/pyinfra/facts/server.py
@@ -567,28 +567,45 @@ class KernelModules(FactBase):
 
     @override
     def command(self):
-        return "! test -f /proc/modules || cat /proc/modules"
+        self._kernel = host.get_fact(Kernel)
+
+        if self._kernel.strip() == "FreeBSD":
+            return "kldstat | tail +2"
+        else:
+            return "! test -f /proc/modules || cat /proc/modules"
 
     default = dict
 
     @override
     def process(self, output):
         modules = {}
+        if self._kernel.strip() == "FreeBSD":
+            for line in output:
+                id, refs, address, size, name = line.split(None, 4)
 
-        for line in output:
-            name, size, instances, depends, state, _ = line.split(" ", 5)
-            instances = int(instances)
+                module = {
+                    "address": address,
+                    "id": id,
+                    "refs": refs,
+                    "size": size,
+                }
 
-            module = {
-                "size": size,
-                "instances": instances,
-                "state": state,
-            }
+                modules[name] = module
+        else:
+            for line in output:
+                name, size, instances, depends, state, _ = line.split(" ", 5)
+                instances = int(instances)
 
-            if depends != "-":
-                module["depends"] = [value for value in depends.split(",") if value]
+                module = {
+                    "size": size,
+                    "instances": instances,
+                    "state": state,
+                }
 
-            modules[name] = module
+                if depends != "-":
+                    module["depends"] = [value for value in depends.split(",") if value]
+
+                modules[name] = module
 
         return modules
 

src/pyinfra/facts/zfs.py
@@ -45,7 +45,7 @@ class ZfsDatasets(FactBase):
         from pyinfra.facts.server import KernelModules
 
         modules = host.get_fact(KernelModules) or {}
-        if "zfs" not in modules:
+        if "zfs" not in modules and "zfs.ko" not in modules:
             return "kernel module 'zfs' is not loaded"
 
     @override