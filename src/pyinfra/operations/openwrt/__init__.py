8c93dc233025ff09840b46b8a2fdc063cfe19fba
morrison12 <morrison12@yahoo.com>
2026-07-14 10:52:32 -0400

    fix: correct __getattr__ docstring for openwrt operations __init__.py
    - operations not facts

--- src/pyinfra/operations/openwrt/__init__.py
+++ src/pyinfra/operations/openwrt/__init__.py
@@ -15,7 +15,7 @@ __all__ = list(__ALL__.keys())
 
 
 def __getattr__(name):
-    # On-demand import of OpenWrt facts, so we don't have to import them all at once
+    # On-demand import of OpenWrt operations, so we don't have to import them all at once
     # this forces py3.7>=, but that's fine as py2 is EOL and py3.6 is also EOL
     # Also, pyinfra is py3.11>=, so this is not a breaking change.
     if name in __all__:
